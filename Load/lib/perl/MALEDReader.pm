d22d55d906f4840a32bfb9ed770b461e8f96139f
Jay Humphrey <jaycolin@apidb.org>
2018-02-16 20:21:40 +0000

    in development
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiData/trunk@87304 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/lib/perl/MALEDReader.pm
@@ -0,0 +1,33 @@
+package ClinEpiData::Load::MALEDReader;
+use base qw(ClinEpiData::Load::MetadataReader);
+
+1;
+
+package ClinEpiData::Load::MALEDReader::ParticipantReader;
+use base qw(ClinEpiData::Load::MALEDReader);
+
+1;
+
+package ClinEpiData::Load::MALEDReader::StoolSampleReader;
+use base qw(ClinEpiData::Load::MALEDReader);
+## loads file micro_x24m.csv
+use strict;
+use warnings;
+
+sub makeParent {
+  my ($self, $hash) = @_;
+  return join("_", $hash->{pid}, $hash->{agedays});
+}
+
+sub makePrimaryKey {
+  my ($self, $hash) = @_;
+  return $hash->{srfmbsampid};
+}
+
+1;
+
+package ClinEpiData::Load::MALEDReader::ObservationReader;
+use base qw(ClinEpiData::Load::MALEDReader);
+## loads files illnessfull_24m.csv, Zscores_24m.csv
+
+1;

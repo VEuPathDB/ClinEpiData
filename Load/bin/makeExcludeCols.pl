5e7f50896f1bd89689e03fc69b28b766617557a1
Jay Humphrey <jaycolin@apidb.org>
2018-05-16 00:10:26 +0000

    A tool for building exclude_cols.txt
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiData/trunk@89057 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/bin/makeExcludeCols.pl
@@ -0,0 +1,91 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use lib $ENV{GUS_HOME} . "/lib/perl";
+
+use ClinEpiData::Load::Owl;
+use Env qw/PROJECT_HOME SPARQLPATH/;
+use Data::Dumper;
+
+my ($dataset,$filter, @files) = @ARGV;
+
+my %columns; ## all columns in data files
+foreach my $file (@files){
+	unless(-f $file){
+		print STDERR "File does not exist: $file\n";
+		next;
+	}
+	open(FH, "<$file") or die "Cannot open $file:$!\n";
+	my $head = <FH>;
+	close(FH);
+	chomp $head;
+	my $delim = ",";
+	$delim = "\t" if($head =~ /\t/);
+	for my $col (split(/$delim/, $head)){
+		$columns{pp($col)} = 1;
+	} 
+}
+
+my $owlFile = "$PROJECT_HOME/ApiCommonData/Load/ontology/release/development/$dataset.owl";
+
+my $owl = ClinEpiData::Load::Owl->new($owlFile);
+
+my $it = $owl->execute('top_level_entities');
+
+my @entities;
+my $filterEntity;
+my %map;
+while (my $row = $it->next) {
+	my $label = pp($row->{label}->as_sparql);
+	my $entity = $row->{entity}->as_sparql;
+	if(defined($filter)){
+		if($label =~ /$filter/i){
+			$filterEntity = $entity;
+			printf STDERR ("$label will be saved\n");
+		}
+		else{
+			push(@entities, $entity);
+			printf STDERR ("$label will be excluded\n");
+		}
+	}
+	$map{$label} = $entity;
+}
+
+unless($filter){
+	printf("Choose one of these top-level entities:\n\t%s\n", join("\n\t", sort keys %map));
+	exit;
+} 
+
+die "Entity not found for $filter\nAvailable:\n\t" . join("\n\t", keys %map) . "\n" unless $filterEntity;
+
+# my @excludes;
+
+foreach my $entity(@entities){
+	my $itr = $owl->execute('all_subclasses', { ENTITY => $entity });
+	my @keys = $itr->binding_names;
+	#printf ("%s\n", join("\t", @keys)) if @keys;
+	while (my $row = $itr->next) {
+		my $col = pp($row->{col}->as_sparql);
+		# push(@excludes, $row->{col}->as_sparql);
+		$columns{$col} = 1;
+	}
+}
+if($filterEntity){
+	my $itr = $owl->execute('all_subclasses', { ENTITY => $filterEntity });
+	my @keys = $itr->binding_names;
+	#printf ("%s\n", join("\t", @keys)) if @keys;
+	while (my $row = $itr->next) {
+		my $col = pp($row->{col}->as_sparql);
+		delete($columns{$col}) if(defined($columns{$col}));
+	}
+}
+	
+print "$_\n" for sort keys %columns;
+
+
+sub pp {
+	my ($val) = @_;
+	$val =~ s/^"|"$//g;
+	return lc($val);
+}

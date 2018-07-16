71c567efee14ad06903178a0f160174e887674c3
Jay Humphrey <jaycolin@apidb.org>
2018-07-16 19:54:13 +0000

    correct functionality of -v
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiData/trunk@90122 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/bin/makeExcludeCols.pl
+++ Load/bin/makeExcludeCols.pl
@@ -7,21 +7,31 @@ use lib $ENV{GUS_HOME} . "/lib/perl";
 use ClinEpiData::Load::Owl;
 use Env qw/PROJECT_HOME SPARQLPATH/;
 use File::Basename;
+use Getopt::Long;
+use Data::Dumper;
 
+my ($dataset, $filter, @files, $inverse);
+GetOptions(
+	'o|owl=s' => \$dataset,
+	'f|filter=s' => \$filter,
+	'i|input=s' => \@files,
+	'v|inverse' => \$inverse
+);
 
-my ($dataset,$filter, @files) = @ARGV;
 
 unless($dataset){
 	printf(join("\n\n",
-		"Usage:\n\t%s [dataset] [[filter]] [[data files ...]]",
-		"Owl file must exist:\$PROJECT_HOME/ApiCommonData/Load/ontology/release/development/[dataset].owl",
+		"Usage:\n\t%s -o|owl [owl] -f|filter [[filter]] -i|input [data file] [ -i [data file ] ] [-v|inverse]",
+		"Owl file must exist:\$PROJECT_HOME/ApiCommonData/Load/ontology/release/development/[owl].owl",
 		"Run without [[filter]] to see a list of options for this dataset",
-		"Run without [[data files ...]] to print only columns that are mapped in this dataset\n"
+		"Run without -i[[data files ...]] to print only columns that are mapped in this dataset",
+		"Run with -v to get only columns in [[filter]]\n"
 	), basename($0));
 	exit;
 }
 
 my %columns; ## all columns in data files
+my %index; ## col => file
 foreach my $file (@files){
 	unless(-f $file){
 		print STDERR "File does not exist: $file\n";
@@ -31,10 +41,12 @@ foreach my $file (@files){
 	my $head = <FH>;
 	close(FH);
 	chomp $head;
+	$head =~ s/\r$//;
 	my $delim = ",";
 	$delim = "\t" if($head =~ /\t/);
 	for my $col (split(/$delim/, $head)){
 		$columns{pp($col)} = 1;
+		$index{pp($col)} = $file;
 	} 
 }
 
@@ -64,7 +76,7 @@ while (my $row = $it->next) {
 }
 
 unless($filter){
-	printf("Choose one of these top-level entities:\n\t%s\n", join("\n\t", sort keys %map));
+	printf STDERR ("Choose one of these top-level entities:\n\t%s\n", join("\n\t", sort keys %map));
 	exit;
 } 
 
@@ -72,27 +84,52 @@ die "Entity not found for $filter\nAvailable:\n\t" . join("\n\t", keys %map) . "
 
 # my @excludes;
 
-foreach my $entity(@entities){
-	my $itr = $owl->execute('all_subclasses', { ENTITY => $entity });
-	my @keys = $itr->binding_names;
-	#printf ("%s\n", join("\t", @keys)) if @keys;
-	while (my $row = $itr->next) {
-		my $col = pp($row->{col}->as_sparql);
-		# push(@excludes, $row->{col}->as_sparql);
-		$columns{$col} = 1;
-	}
-}
+# foreach my $entity(@entities){
+# 	my $itr = $owl->execute('all_subclasses', { ENTITY => $entity });
+# 	my @keys = $itr->binding_names;
+# 	#printf ("%s\n", join("\t", @keys)) if @keys;
+# 	while (my $row = $itr->next) {
+# 		my $col = pp($row->{col}->as_sparql);
+# 		# push(@excludes, $row->{col}->as_sparql);
+# 		$columns{$col} = 1;
+# 	}
+# }
+
+my %saved;
 if($filterEntity){
 	my $itr = $owl->execute('all_subclasses', { ENTITY => $filterEntity });
-	my @keys = $itr->binding_names;
+	#my @keys = $itr->binding_names;
 	#printf ("%s\n", join("\t", @keys)) if @keys;
 	while (my $row = $itr->next) {
 		my $col = pp($row->{col}->as_sparql);
-		delete($columns{$col}) if(defined($columns{$col}));
+		if(defined($columns{$col})){
+			delete($columns{$col});
+			$saved{$col} = 1;
+		}
 	}
 }
-	
-print "$_\n" for sort keys %columns;
+
+print STDERR ("-------------------\n");
+
+if($inverse){
+	foreach my $k (sort keys %saved){
+		print "$k\n";
+	}
+	print "___UNMAPPED_COLS___\n";
+	foreach my $col (sort keys %index){
+		print "$col\n" unless $saved{$col};
+	}
+}
+else{
+	print "$_\n" for sort keys %columns;
+	## also exclude any columns not found in owl
+	print "___UNMAPPED_COLS___\n";
+	foreach my $col (sort keys %index){
+		print "$col\n" unless $columns{$col};
+	}
+}
+
+exit;
 
 
 sub pp {
@@ -100,3 +137,5 @@ sub pp {
 	$val =~ s/^"|"$//g;
 	return lc($val);
 }
+
+1;

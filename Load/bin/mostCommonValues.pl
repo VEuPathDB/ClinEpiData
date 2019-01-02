04b7705421c36faef0f1b5924416fce8582789e3
Jay Humphrey <jaycolin@apidb.org>
2019-01-02 15:30:09 +0000

    script for merging conflicts in a data file
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiData/trunk@94183 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Load/bin/mostCommonValues.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Getopt::Long;
+
+my ($file, @keys, @cols);
+GetOptions(
+	'f|file=s' => \$file,
+	'k|key=s' => \@keys, # columns to use for primary key
+	'c|col=s' => \@cols, # columns of data to merge
+);
+my $merge = {}; 
+open (FH, "<$file") or die "$!\n";
+my $line = <FH>;
+chomp $line;
+my @index = split(/\t/, $line);
+while($line = <FH>){
+	chomp $line;
+	my @row = split(/\t/, $line);
+	my %data;
+	@data{@index} = @row;
+	my $key = join("", @data{@keys});
+	$merge->{$key} ||= {};
+	foreach my $col (@cols){
+		next if $data{$col} eq "";
+		$merge->{$key}->{$col} ||= { _total => 0, $data{$col} => 0 };
+		$merge->{$key}->{$col}->{$data{$col}}++;
+		$merge->{$key}->{$col}->{_total} = scalar keys %{$merge->{$key}->{$col}} ;
+	}
+}
+close(FH);
+while(my ($key,$data) = each %$merge){
+	foreach my $col(@cols){
+		next unless $merge->{$key}->{$col}->{_total} > 1;
+		delete $merge->{$key}->{$col}->{_total};
+		my $scores = $merge->{$key}->{$col};
+		my ($topvalue) = sort { $scores->{$b} <=> $scores->{$a} } keys %$scores ;
+		$merge->{$key}->{$col} = $topvalue;
+	}
+}
+while(my ($key,$data) = each %$merge){
+	my @row = map { $data->{$_} } @cols;
+	printf("%s\t%s\n", $key, join("\t", @row));
+}
+		

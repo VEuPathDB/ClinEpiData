c17fa7abbfe40b7de3420a55526836dc0afe2860
Jay Humphrey <jaycolin@apidb.org>
2019-01-24 18:33:09 +0000

    some changes for loading post-phase2 but keeping 24-month range (eventually will be 3 years)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiData/trunk@94782 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/MALEDReader.pm
+++ Load/lib/perl/MALEDReader.pm
@@ -18,6 +18,7 @@ sub cleanAndAddDerivedData {
 	}
 	
 }
+
 1;
 
 package ClinEpiData::Load::MALEDReader::HouseholdReader;
@@ -48,6 +49,14 @@ sub getPrimaryKeyPrefix {
 1;
 package ClinEpiData::Load::MALEDReader::ParticipantReader;
 use base qw(ClinEpiData::Load::MALEDReader);
+
+sub cleanAndAddDerivedData {
+  my ($self, $hash) = @_;
+  $self->SUPER::cleanAndAddDerivedData($hash);
+	if(!defined($hash->{gender}) || $hash->{gender} eq ""){
+		$hash->{gender} = $hash->{cafsex};
+	}
+}
 
 sub makeParent {
   my ($self, $hash) = @_;
@@ -149,6 +158,14 @@ sub cleanAndAddDerivedData {
 		$hash->{sample_target_month} = $hash->{target_month};
 	 	delete $hash->{target_month};
 	}
+	for my $col (qw/srfconsist srfstblood/){
+		delete($hash->{$col}) if(defined($hash->{$col}) && $hash->{$col} =~ /NA/i);
+	}
+	for my $col (qw/date datecol srfdate ucfdate/ ){
+		if(defined($hash->{$col} && $hash->{$col} ne "")){
+			$hash->{sample_collection_date} = $hash->{$col};
+		}
+	}
 }
 
 1;
@@ -158,6 +175,35 @@ use base qw(ClinEpiData::Load::MALEDReader);
 use strict;
 use warnings;
 use Data::Dumper;
+
+my @obs_date_cols = (
+"bcgdate1", "bcgdate2", "cpoxdate1", "dewormdate1", "dewormdate2", "dptdate1", "dptdate2", "dptdate3", "dptdate4", "dptdate5", "fludate1", "fludate2",
+"fludate3", "fludate4", "fludate5", "frqdate", "fsedate", "fsqdate", "hepbdate1", "hepbdate2", "hepbdate3", "hepbdate4", "hepbdate5", "hibdate1",
+"hibdate2", "hibdate3", "hibdate4", "hibdate5", "ipvdate1", "ipvdate2", "ipvdate3", "ipvdate4", "ipvdate5", "jedate1", "meadate1", "meadate2",
+"meadate3", "meadate4", "mendate1", "mendate2", "mendate3", "mmrdate1", "mmrdate2", "mmrdate3", "moadate", "mumdate1", "mumdate2", "mumdate3",
+"opvdate1", "opvdate10", "opvdate11", "opvdate12", "opvdate13", "opvdate14", "opvdate15", "opvdate16", "opvdate17", "opvdate18", "opvdate19", "opvdate2",
+"opvdate20", "opvdate21", "opvdate22", "opvdate23", "opvdate24", "opvdate25", "opvdate3", "opvdate4", "opvdate5", "opvdate6", "opvdate7", "opvdate8",
+"opvdate9", "pcvdate1", "pcvdate2", "pcvdate3", "pcvdate4", "rabdate1", "rabdate2", "rabdate3", "rabdate4", "rabdate5", "rotadate1", "rotadate2",
+"rotadate3", "rubdate1", "rubdate2", "rubdate3", "tetadate1", "tetadate2", "tetadate3", "tetadate4", "tetadate5", "tetadate6", "tetadate7", "tetadate8",
+"tetadate9", "typdate1", "vitadate1", "vitadate2", "vitadate3", "vitadate4", "vitadate5", "vitadate6", "vitadate7", "yfdate1");
+my @vac_date_cols = (
+"bcgdate1", "bcgdate2", "cpoxdate1", "dptdate1", "dptdate2", "dptdate3", "dptdate4", "dptdate5", "fludate1", "fludate2",
+"fludate3", "fludate4", "fludate5", "hepbdate1", "hepbdate2", "hepbdate3", "hepbdate4", "hepbdate5", "hibdate1",
+"hibdate2", "hibdate3", "hibdate4", "hibdate5", "ipvdate1", "ipvdate2", "ipvdate3", "ipvdate4", "ipvdate5", "jedate1", "meadate1", "meadate2",
+"meadate3", "meadate4", "mendate1", "mendate2", "mendate3", "mmrdate1", "mmrdate2", "mmrdate3", "mumdate1", "mumdate2", "mumdate3",
+"opvdate1", "opvdate10", "opvdate11", "opvdate12", "opvdate13", "opvdate14", "opvdate15", "opvdate16", "opvdate17", "opvdate18", "opvdate19", "opvdate2",
+"opvdate20", "opvdate21", "opvdate22", "opvdate23", "opvdate24", "opvdate25", "opvdate3", "opvdate4", "opvdate5", "opvdate6", "opvdate7", "opvdate8",
+"opvdate9", "pcvdate1", "pcvdate2", "pcvdate3", "pcvdate4", "rabdate1", "rabdate2", "rabdate3", "rabdate4", "rabdate5", "rotadate1", "rotadate2",
+"rotadate3", "rubdate1", "rubdate2", "rubdate3", "tetadate1", "tetadate2", "tetadate3", "tetadate4", "tetadate5", "tetadate6", "tetadate7", "tetadate8",
+"tetadate9", "typdate1", "yfdate1");
+
+sub normalizeDate {
+	my ($self, $val) = @_;
+	$val =~ s/^(\d+)[-\/]?([a-z]{3})[-\/]?(\d+)$/$1-$2-$3/;
+	$val =~ s/-(\d\d)$/-20$1/; 
+	$val =~ s/^(\d)-/0$1-/;
+	return $val;
+}
 
 sub makeParent {
   ## returns a Participant ID
@@ -215,6 +261,20 @@ sub cleanAndAddDerivedData {
 			$hash->{"${type}_target_month"} = $hash->{target_month};
 		}
 	}
+	for my $col (@obs_date_cols){
+		if(defined($hash->{$col})){
+			$hash->{observation_date} = $self->normalizeDate($hash->{$col});
+		}
+	}
+	for my $col (@vac_date_cols){
+		if(defined($hash->{$col})){
+			$hash->{vaccination_date} = $self->normalizeDate($hash->{$col});
+		}
+	}
+	if($mdFile =~ /zscores/i && defined($hash->{date})){
+		$hash->{anthro_date} = $hash->{date};
+	}
+	
 }
 
 package ClinEpiData::Load::MALEDReader::HouseholdObservationReader;
@@ -223,7 +283,7 @@ use base qw(ClinEpiData::Load::MALEDReader::ObservationReader);
 sub cleanAndAddDerivedData {
   my ($self, $hash) = @_;
   $self->SUPER::cleanAndAddDerivedData($hash);
-  my $mdFile = basename($self->getMetadataFile());
+  my $mdFile = $self->getMetadataFile();
 	for my $type (qw/fsq wami/){
 		if($mdFile =~ /$type/i){
 			$hash->{"${type}_target_month"} = $hash->{target_month};

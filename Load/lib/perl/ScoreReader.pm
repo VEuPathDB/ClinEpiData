f793f5ae8d9a872f70535bb392ae76246df6c27c
Jay Humphrey <jaycolin@apidb.org>
2019-02-01 20:29:04 +0000

    use rowMultiplier() for sample rows
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiData/trunk@94988 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/ScoreReader.pm
+++ Load/lib/perl/ScoreReader.pm
@@ -109,74 +109,18 @@ use strict;
 use File::Basename;
 
 
-sub read {
-  my ($self) = @_;
-  my $mdfile = $self->getMetadataFile();
-  my $fileBasename = basename($mdfile);
-  my $rowExcludes = $self->getRowExcludes();
-  my $colExcludes = $self->getColExcludes();
-  open(FILE, $mdfile) or die "Cannot open file $mdfile for reading: $!";
-  my $header = <FILE>;
-  chomp $header;
-  my $delimiter = $self->getDelimiter($header);
-  my @headers = $self->splitLine($delimiter, $header);
-  my $headersAr = $self->adjustHeaderArray(\@headers);
-  $headersAr = $self->clean($headersAr);
-  my $parsedOutput = {};
-  while(<FILE>) {
-    chomp;
-    my @values = $self->splitLine($delimiter, $_);
-    my $valuesAr = $self->clean(\@values);
-    foreach my $specnum (qw/1 2 3/){
-      foreach my $abslide (qw/a b/){
-        my %hash;
-        for(my $i = 0; $i < scalar @$headersAr; $i++) {
-          my $key = lc($headersAr->[$i]);
-          my $value = lc($valuesAr->[$i]);
-          next if($value eq '[skipped]');
-          $hash{$key} = $value if(defined $value);
-        }
-        $hash{'specimen number'} = $specnum;
-        $hash{'a or b slide'} = $abslide;
-        my $primaryKey = $self->makePrimaryKey(\%hash);
-        my $parent = $self->makeParent(\%hash);
-        next if($self->skipIfNoParent() && !$parent);
-        my $parentPrefix = $self->getParentPrefix(\%hash);
-        my $parentWithPrefix = $parentPrefix . $parent;
-        $hash{'__PARENT__'} = $parentWithPrefix unless($parentPrefix && $parentWithPrefix eq $parentPrefix);
-        next unless($primaryKey); # skip rows that do not have a primary key
-        if(defined($rowExcludes->{lc($primaryKey)}) && ($rowExcludes->{lc($primaryKey)} eq $fileBasename) || ($rowExcludes->{lc($primaryKey)} eq '__ALL__')){
-          next;
-        }
-				my $prefix = $self->getPrimaryKeyPrefix(\%hash);
-        if($prefix) { $primaryKey = $prefix . $primaryKey; }
-        if(defined($rowExcludes->{lc($primaryKey)}) && ($rowExcludes->{lc($primaryKey)} eq $fileBasename) || ($rowExcludes->{lc($primaryKey)} eq '__ALL__')){
-          next;
-        }
-        $self->cleanAndAddDerivedData(\%hash);
-        foreach my $key (keys %hash) {
-          next if(defined($colExcludes->{lc($primaryKey)}) && $colExcludes->{$fileBasename}->{$key} || $colExcludes->{'__ALL__'}->{$key});
-          next unless defined $hash{$key}; # skip undef values
-          next if($hash{$key} eq '');
-          next if($self->seen($parsedOutput->{$primaryKey}->{$key}, $hash{$key}));
-          push @{$parsedOutput->{$primaryKey}->{$key}}, $hash{$key};
-        }
-      }
+sub rowMultiplier {
+  my ($self, $hash) = @_;
+  my @multi;
+  foreach my $specnum (qw/1 2 3/){
+    foreach my $abslide (qw/a b/){
+      my %clone = ( %$hash  );
+      $clone{'specimen number'} = $specnum;
+      $clone{'a or b slide'} = $abslide;
+      push(@multi, \%clone);
     }
   }
-  close FILE;
-  my $rv = {};
-  foreach my $primaryKey (keys %$parsedOutput) {
-    foreach my $key (keys %{$parsedOutput->{$primaryKey}}) {
-      my @values = @{$parsedOutput->{$primaryKey}->{$key}};
-      for(my $i = 0; $i < scalar @values; $i++) {
-        my $value = $values[$i];
-        my $newKey = $i == 0 ? $key : "${key}_$i";
-        $rv->{$primaryKey}->{$newKey} = $values[$i];
-      }
-    }
-  }
-  $self->setParsedOutput($rv);
+  return \@multi;
 }
 
 sub cleanAndAddDerivedData {

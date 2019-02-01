e22af9aa03ffdbc682bb62f8046457d77d539b6a
Jay Humphrey <jaycolin@apidb.org>
2019-02-01 20:26:29 +0000

    refactor read() to support rowMultiplier() which can be used to build several rows new from one row
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiData/trunk@94987 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/MetadataReader.pm
+++ Load/lib/perl/MetadataReader.pm
@@ -37,6 +37,8 @@ sub applyAncillaryData {
   die "Ancillary File provided bun no method implemented to use it.";
 }
 
+sub rowMultiplier { return [ $_[1] ]; }
+
 sub seen {
   my ($self, $ar, $v) = @_;
 
@@ -116,106 +118,70 @@ sub splitLine {
 
 sub read {
   my ($self) = @_;
-
   my $metadataFile = $self->getMetadataFile();
-
+  my $fileBasename = basename $metadataFile;
   my $colExcludes = $self->getColExcludes();
   my $rowExcludes = $self->getRowExcludes();
-
-  my $fileBasename = basename $metadataFile;
-
   open(FILE, $metadataFile) or die "Cannot open file $metadataFile for reading: $!";
-
   my $header = <FILE>;
   $header =~s/\n|\r//g;
-
   my $delimiter = $self->getDelimiter($header);
-
   my @headers = $self->splitLine($delimiter, $header);
-
   my $headersAr = $self->adjustHeaderArray(\@headers);
-
   $headersAr = $self->clean($headersAr);
-
-
-
   my $parsedOutput = {};
-
   while(<FILE>) {
     $_ =~ s/\n|\r//g;
-
     my @values = $self->splitLine($delimiter, $_);
     my $valuesAr = $self->clean(\@values);
-
-    my %hash;
+    my %rowData;
     for(my $i = 0; $i < scalar @$headersAr; $i++) {
       my $key = lc($headersAr->[$i]);
       my $value = lc($valuesAr->[$i]);
-
-      # TODO: move this to PRISM class clean method
       next if($value eq '[skipped]');
-
-      $hash{$key} = $value if(defined $value);
+      $rowData{$key} = $value if(defined $value);
     }
-
-    my $primaryKey = $self->makePrimaryKey(\%hash);
-    my $parent = $self->makeParent(\%hash);
-
-    next if($self->skipIfNoParent() && !$parent);
-
-    my $parentPrefix = $self->getParentPrefix(\%hash);
-    my $parentWithPrefix = $parentPrefix . $parent;
-
-    $hash{'__PARENT__'} = $parentWithPrefix unless($parentPrefix && $parentWithPrefix eq $parentPrefix);
-
-    next unless($primaryKey); # skip rows that do not have a primary key
-    if(($rowExcludes->{$primaryKey} eq $fileBasename) || ($rowExcludes->{$primaryKey} eq '__ALL__')){
-			next;
-		}
-
-    $primaryKey = $self->getPrimaryKeyPrefix(\%hash) . $primaryKey;
-
-    if(($rowExcludes->{$primaryKey} eq $fileBasename) || ($rowExcludes->{$primaryKey} eq '__ALL__')){
-			next;
-		}
-
-    $self->cleanAndAddDerivedData(\%hash);
-
-    foreach my $key (keys %hash) {
-      next if($colExcludes->{$fileBasename}->{$key} || $colExcludes->{'__ALL__'}->{$key});
-      next unless defined $hash{$key}; # skip undef values
-      next if($hash{$key} eq '');
-
-      next if($self->seen($parsedOutput->{$primaryKey}->{$key}, $hash{$key}));
-
-      push @{$parsedOutput->{$primaryKey}->{$key}}, $hash{$key};
+    my $rowMulti = $self->rowMultiplier(\%rowData);
+    foreach my $hash ( @$rowMulti ) {
+       my $primaryKey = $self->makePrimaryKey($hash);
+       my $parent = $self->makeParent($hash);
+       next if($self->skipIfNoParent() && !$parent);
+       my $parentPrefix = $self->getParentPrefix($hash);
+       my $parentWithPrefix = $parentPrefix . $parent;
+       $hash->{'__PARENT__'} = $parentWithPrefix unless($parentPrefix && $parentWithPrefix eq $parentPrefix);
+       next unless($primaryKey); # skip rows that do not have a primary key
+       if(defined($rowExcludes->{lc($primaryKey)}) && ($rowExcludes->{lc($primaryKey)} eq $fileBasename) || ($rowExcludes->{lc($primaryKey)} eq '__ALL__')){
+         next;
+       }
+       my $prefix = $self->getPrimaryKeyPrefix($hash);
+       if($prefix) { $primaryKey = $prefix . $primaryKey; }
+       if(defined($rowExcludes->{lc($primaryKey)}) && ($rowExcludes->{lc($primaryKey)} eq $fileBasename) || ($rowExcludes->{lc($primaryKey)} eq '__ALL__')){
+         next;
+       }
+       $self->cleanAndAddDerivedData($hash);
+       foreach my $key (keys %$hash) {
+         next if(defined($colExcludes->{lc($primaryKey)}) && $colExcludes->{$fileBasename}->{$key} || $colExcludes->{'__ALL__'}->{$key});
+         next unless defined $hash->{$key}; # skip undef values
+         next if($hash->{$key} eq '');
+         next if($self->seen($parsedOutput->{$primaryKey}->{$key}, $hash->{$key}));
+         push @{$parsedOutput->{$primaryKey}->{$key}}, $hash->{$key};
+       }
     }
-
   }
-
   close FILE;
-
   my $rv = {};
-
   foreach my $primaryKey (keys %$parsedOutput) {
-
     foreach my $key (keys %{$parsedOutput->{$primaryKey}}) {
-
       my @values = @{$parsedOutput->{$primaryKey}->{$key}};
-
       for(my $i = 0; $i < scalar @values; $i++) {
         my $value = $values[$i];
-
         my $newKey = $i == 0 ? $key : "${key}_$i";
         $rv->{$primaryKey}->{$newKey} = $values[$i];
       }
     }
   }
-
-
   $self->setParsedOutput($rv);
 }
-
 
 sub makePrimaryKey {
   die "SUBCLASS must override makePrimaryKey method";

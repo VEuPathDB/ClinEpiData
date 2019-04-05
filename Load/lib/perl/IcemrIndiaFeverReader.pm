987b9ea9157f3ac8c3d4bdcf97f014afc337ea65
Jay Humphrey <jaycolin@apidb.org>
2019-04-05 20:28:44 +0000

    misc updates
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiData/trunk@97103 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/IcemrIndiaFeverReader.pm
+++ Load/lib/perl/IcemrIndiaFeverReader.pm
@@ -1,5 +1,7 @@
 package ClinEpiData::Load::IcemrIndiaFeverReader;
 use base qw(ClinEpiData::Load::MetadataReader);
+use File::Basename;
+use Data::Dumper;
 
 #sub cleanAndAddDerivedData {
 #  my ($self, $hash) = @_;
@@ -7,9 +9,12 @@ use base qw(ClinEpiData::Load::MetadataReader);
 
 sub getId {
   my ($self, $hash) = @_;
-	for my $idcol (qw/sid igm_chik_sid1 igm_typhus_sid1 mic_sid1 ns1_dengue_sid1 pcr_sid1 rdt_sid1/){
-		return $hash->{$idcol} if defined $hash->{$idcol};
-	}
+  for my $idcol (qw/sid igm_chik_sid1 igm_typhus_sid1 mic_sid1 ns1_dengue_sid1 pcr_sid1 rdt_sid1/){
+    if(defined($hash->{$idcol}) && $hash->{$idcol} ne ""){
+    	return $hash->{$idcol};
+		}
+  }
+	die "Cannot get ID for $mdfile:\n" . Dumper($hash);
 }
 1;
 
@@ -19,9 +24,9 @@ use base qw(ClinEpiData::Load::IcemrIndiaFeverReader);
 # sub cleanAndAddDerivedData {
 #   my ($self, $hash) = @_;
 #   if(defined($hash->{pastyear_treatdate})){
-# 		$hash->{pastyear_treat_date} = $hash->{pastyear_treatdate};
-# 		delete $hash->{pastyear_treatdate};
-# 	}
+#     $hash->{pastyear_treat_date} = $hash->{pastyear_treatdate};
+#     delete $hash->{pastyear_treatdate};
+#   }
 #   if(defined($hash->{pastyear_treat_date})){
 #     my $val = $hash->{pastyear_treat_date};
 #     $val =~ tr/_/ /; ## some dates look like mmm_YYYY
@@ -35,13 +40,13 @@ use base qw(ClinEpiData::Load::IcemrIndiaFeverReader);
 
 sub makeParent {
 #  my ($self, $hash) = @_;
-#	return $hash->{parent} if defined $hash->{parent};
+# return $hash->{parent} if defined $hash->{parent};
 #  return $self->getId($hash);
 }
 # sub getParentPrefix {
 #   my ($self, $hash) = @_;
-# 	return undef if defined $hash->{parent};
-# 	return "HH";
+#   return undef if defined $hash->{parent};
+#   return "HH";
 # }
 sub makePrimaryKey {
   my ($self, $hash) = @_;
@@ -59,17 +64,17 @@ sub makePrimaryKey {
 #package ClinEpiData::Load::IcemrIndiaFeverReader::HouseholdReader;
 #use base qw(ClinEpiData::Load::IcemrIndiaFeverReader::ParticipantReader);
 #sub makeParent {
-#	return undef;
+# return undef;
 #}
 #
 #sub getParentPrefix {
-#	return undef;
+# return undef;
 #}
 #
 #sub getPrimaryKeyPrefix {
 #  my ($self, $hash) = @_;
-#	return undef if defined $hash->{parent};
-#	return "HH";
+# return undef if defined $hash->{parent};
+# return "HH";
 #}
 #
 #1;
@@ -82,10 +87,12 @@ use Data::Dumper;
 
 sub cleanAndAddDerivedData {
   my ($self, $hash) = @_;
-	if(defined($hash->{bp})){
-		($hash->{systolic_bp},$hash->{diastolic_bp}) = split(/\//, $hash->{bp});
-		delete($hash->{bp});
-	}
+  if(defined($hash->{bp})){
+    ($hash->{systolic_bp},$hash->{diastolic_bp}) = split(/\//, $hash->{bp});
+    delete($hash->{bp});
+  }
+	$hash->{date} ||= $hash->{date_fu};
+	$hash->{date} ||= $hash->{date_eos};
 }
 
 sub makeParent {
@@ -103,12 +110,12 @@ sub makePrimaryKey {
     return $hash->{"primary_key"};
   }
   my $id = $self->getId($hash);
-#	my $file = basename($self->getMetadataFile());
-#	my $field = $datefield{$file};
-#	die "No date field for $file" unless $field;
+# my $file = basename($self->getMetadataFile());
+# my $field = $datefield{$file};
+# die "No date field for $file" unless $field;
 #  return join("_", $id, $field);
-	my $event = substr($hash->{redcap_event_name},0,3);
-	return join("_", $id, $event);
+  my $event = substr($hash->{redcap_event_name},0,3);
+  return join("_", $id, $event);
 }
 
 1;
@@ -116,9 +123,9 @@ sub makePrimaryKey {
 #use base qw(ClinEpiData::Load::IcemrIndiaFeverReader::ObservationReader);
 #sub cleanAndAddDerivedData {
 #  my ($self, $hash) = @_;
-#	if($hash->{redcap_event_name} eq 'enrollment_arm_1'){
-#		delete $hash->{$_} for keys %$hash;
-#	}
+# if($hash->{redcap_event_name} eq 'enrollment_arm_1'){
+#   delete $hash->{$_} for keys %$hash;
+# }
 #}
 #sub makeParent {
 #  ## returns a Participant ID
@@ -127,7 +134,7 @@ sub makePrimaryKey {
 #    return $hash->{"parent"};
 #  }
 #  my $id = $self->getId($hash);
-#	return join("_", $id, 'enrollment_arm_1');
+# return join("_", $id, 'enrollment_arm_1');
 #}
 #
 #1;
@@ -135,62 +142,117 @@ sub makePrimaryKey {
 package ClinEpiData::Load::IcemrIndiaFeverReader::SampleReader;
 use base qw(ClinEpiData::Load::IcemrIndiaFeverReader);
 use File::Basename;
-my %source = (
-'IGHFeverCRF_DATA_2018-09-12_1813.csv' => 'crfdata',
-'IGHFeverCRF_DATA_2018-09-12_1813.txt' => 'crfdata',
-'IGHFeverELISAChikung_DATA_2018-09-12_1817.csv' => 'elisachikung',
-'IGHFeverELISADengueI_DATA_2018-09-12_1819.csv' => 'elisadenguei',
-'IGHFeverELISADengueN_DATA_2018-09-12_1819.csv' => 'elisadenguen',
-'IGHFeverELISAScrubty_DATA_2018-09-12_1818.csv' => 'elisyscrubty',
-'IGHFeverMxMalaria_DATA_2018-09-12_1821.csv' => 'mxmalaria',
-'IGHFeverPCRMalaria_DATA_2018-11-14_2100.csv' => 'pcrmalaria',
-'IGHFeverPCRSTyphi_DATA_2018-09-12_1812.csv' => 'pcrstyphi',
-'IGHFeverRDTDengueChi_DATA_2018-09-12_1816.csv' => 'rdtdenguechi',
-);
+use Data::Dumper;
+# my %source = (
+# 'IGHFeverCRF_DATA_2018-09-12_1813.csv' => 'crfdata',
+# 'IGHFeverCRF_DATA_2018-09-12_1813.txt' => 'crfdata',
+# 'IGHFeverELISAChikung_DATA_2018-09-12_1817.csv' => 'elisachikung',
+# 'IGHFeverELISADengueI_DATA_2018-09-12_1819.csv' => 'elisadenguei',
+# 'IGHFeverELISADengueN_DATA_2018-09-12_1819.csv' => 'elisadenguen',
+# 'IGHFeverELISAScrubty_DATA_2018-09-12_1818.csv' => 'elisyscrubty',
+# 'IGHFeverMxMalaria_DATA_2018-09-12_1821.csv' => 'mxmalaria',
+# 'IGHFeverPCRMalaria_DATA_2018-11-14_2100.csv' => 'pcrmalaria',
+# 'IGHFeverPCRSTyphi_DATA_2018-09-12_1812.csv' => 'pcrstyphi',
+# 'IGHFeverRDTDengueChi_DATA_2018-09-12_1816.csv' => 'rdtdenguechi',
+# );
 sub cleanAndAddDerivedData {
   my ($self, $hash) = @_;
-	my $mdfile = basename($self->getMetadataFile());
-	if($mdfile =~ /IGHFeverMxMalaria_DATA/){
-##			mic_counts1 needs to be split into 4 different variables:
-##			If the value for mic_species___2 is 1, 
-##			then the number before the “/” maps to “Plasmodium falciparum asexual stage density (per uL blood), by microscopy” EUPATH_0000550 pfal_a
-##			and the number after the “/” maps to “Plasmodium falciparum gametocyte density (per uL blood), by microscopy” EUPATH_0000546 pfal_g
-##			If the value for mic_species___3 is 1,
-##			then the number before the “/” maps to “Plasmodium vivax asexual stage density (per uL blood), by microscopy” EUPATH_0000551 pviv_a
-##			and the number after the “/” maps to “Plasmodium vivax gametocyte density (per uL blood), by microscopy” EUPATH_0000547 pviv_g
+	return unless (scalar keys %$hash);
+  my $mdfile = basename($self->getMetadataFile());
+  if($mdfile =~ /IGHFeverMxMalaria_DATA/){
+##      mic_counts1 needs to be split into 4 different variables:
+##      If the value for mic_species___2 is 1, 
+##      then the number before the “/” maps to “Plasmodium falciparum asexual stage density (per uL blood), by microscopy” EUPATH_0000550 pfal_a
+##      and the number after the “/” maps to “Plasmodium falciparum gametocyte density (per uL blood), by microscopy” EUPATH_0000546 pfal_g
+##      If the value for mic_species___3 is 1,
+##      then the number before the “/” maps to “Plasmodium vivax asexual stage density (per uL blood), by microscopy” EUPATH_0000551 pviv_a
+##      and the number after the “/” maps to “Plasmodium vivax gametocyte density (per uL blood), by microscopy” EUPATH_0000547 pviv_g
 
-		my @mc1 = split(/\//, $hash->{mic_counts1});
-		if($hash->{mic_species___2}){
-			($hash->{pfal_a},$hash->{pfal_g}) = @mc1;
+    my @mc1 = split(/\//, $hash->{mic_counts1});
+    if($hash->{mic_species___2}){
+      ($hash->{pfal_a},$hash->{pfal_g}) = @mc1;
+    }
+    elsif($hash->{mic_species___3}){
+      ($hash->{pviv_a},$hash->{pviv_g}) = @mc1;
+    }
+  }
+  $self->mergeSCD($hash) if(!defined($hash->{sample_collection_date}));
+  if(!defined($hash->{sample_collection_date})){
+    my $pid = $self->makeParent($hash);
+		die "No parent\n" . Dumper $hash unless $pid;
+		print "Parent=$pid\n";
+    my $ppo = $self->getParentParsedOutput();
+		foreach my $alt (qw/date date_eos date_fu/){
+			if(defined($ppo->{$pid}->{$alt}) && $ppo->{$pid}->{$alt} ne ""){
+    		$hash->{sample_collection_date} = $ppo->{$pid}->{$alt};
+				last;
+			}
 		}
-		elsif($hash->{mic_species___3}){
-			($hash->{pviv_a},$hash->{pviv_g}) = @mc1;
-		}
-	}
+  }
+  if(!defined($hash->{sample_collection_date})){
+    my $id = $self->makePrimaryKey($hash);
+    die "$mdfile:$id has no scd\n";
+  }
 }
 sub makeParent {
   ## returns a Participant ID
   my ($self, $hash) = @_;
+	return unless(scalar keys %$hash);
   if($hash->{"parent"}) {
     return $hash->{"parent"};
   }
   my $id = $self->getId($hash);
-	my $event = substr($hash->{redcap_event_name} || 'enrollment_arm_1', 0, 3);
-	return join("_", $id, $event);
+	unless($id){
+  	my $mdfile = $self->getMetadataFile();
+		("No id found in $mdfile\n" . Dumper $hash);
+	}
+  my $event = substr($hash->{redcap_event_name} || 'enrollment_arm_1', 0, 3);
+  return join("_", $id, $event);
 }
 
 sub makePrimaryKey {
-  ## returns a Participant ID
   my ($self, $hash) = @_;
   if($hash->{"primary_key"}) {
     return $hash->{"primary_key"};
   }
   my $id = $self->getId($hash);
-	#my $field = $datefield{basename($self->getMetadataFile())};
- 	my $event = substr($hash->{redcap_event_name},0,3);
-#	return join("_", $id, $event) ;
-	my $src = $source{basename($self->getMetadataFile())};
-	return join("_", $id, $src, $event);
+  #my $field = $datefield{basename($self->getMetadataFile())};
+  #my $event = substr($hash->{redcap_event_name},0,3);
+  my $event = substr($hash->{redcap_event_name} || 'enrollment_arm_1', 0, 3);
+	my $date = "NA";
+  if(!defined($hash->{sample_collection_date})){
+  	$date = $self->mergeSCD($hash);
+	}
+  # return join("_", $id, $event) ;
+  return join("_", $id, $date, $event);
+  my $src = $source{basename($self->getMetadataFile())};
+  return join("_", $id, $src, $event);
+}
+sub getPrimaryKeyPrefix {
+  return "S";
+}
+
+sub mergeSCD {
+  my ($self, $hash) = @_;
+  if(defined($hash->{date}) && $hash->{date} ne ""){ # CRF
+    $hash->{sample_collection_date} = $hash->{date};
+    return $hash->{sample_collection_date};
+  }
+
+  for my $dates (qw/igm_chik_scd igm_typhus_scd mic_sample_collect_date ns1_dengue_scd rdt_scd/){
+    if(defined($hash->{$dates}) && $hash->{$dates} ne ""){
+      eval { $hash->{sample_collection_date} = $self->formatDate($hash->{$dates}); };
+      print "Bad NON-US date format: $mdfile: " . $self->makePrimaryKey($hash) . "\n" if $@;
+    	return $hash->{sample_collection_date};
+    }
+  }
+  for my $dates (qw/igm_dengue_scd pcr_scd/){
+    if(defined($hash->{$dates}) && $hash->{$dates} ne ""){
+      eval { $hash->{sample_collection_date} = $self->formatDate($hash->{$dates}, 'US'); };
+      print "Bad US date format: $mdfile: " . $self->makePrimaryKey($hash) . "\n" if $@;
+    	return $hash->{sample_collection_date};
+    }
+  }
 }
 
 1;
@@ -199,12 +261,12 @@ sub makePrimaryKey {
 #use base qw(ClinEpiData::Load::IcemrIndiaFeverReader::SampleReader);
 #sub cleanAndAddDerivedData {
 #  my ($self, $hash) = @_;
-#	unless(defined($hash->{redcap_event_name}) && $hash->{redcap_event_name}){
-#		$hash->{redcap_event_name} = 'enrollment_arm_1';
-#	}
-#	if($hash->{redcap_event_name} eq 'enrollment_arm_1'){
-#		delete $hash->{$_} for keys %$hash;
-#	}
+# unless(defined($hash->{redcap_event_name}) && $hash->{redcap_event_name}){
+#   $hash->{redcap_event_name} = 'enrollment_arm_1';
+# }
+# if($hash->{redcap_event_name} eq 'enrollment_arm_1'){
+#   delete $hash->{$_} for keys %$hash;
+# }
 #}
 #
 #sub makePrimaryKey {
@@ -214,9 +276,9 @@ sub makePrimaryKey {
 #    return $hash->{"primary_key"};
 #  }
 #  my $id = $self->getId($hash);
-#	#my $field = $datefield{basename($self->getMetadataFile())};
-#	my $event = $hash->{redcap_event_name};
-#	return join("_", $id, $event, "S") ;
+# #my $field = $datefield{basename($self->getMetadataFile())};
+# my $event = $hash->{redcap_event_name};
+# return join("_", $id, $event, "S") ;
 #}
 #
 #1;

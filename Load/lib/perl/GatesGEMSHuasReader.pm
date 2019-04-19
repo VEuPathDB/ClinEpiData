0d24cc39ff358f6c757c8b058a082cdeb3dea91c
Lin Xu <linxu123@upenn.edu>
2019-04-19 17:08:19 +0000

    GEMS HUAS 1/1a Observation Reader update
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiData/trunk@97432 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/GatesGEMSHuasReader.pm
+++ Load/lib/perl/GatesGEMSHuasReader.pm
@@ -134,25 +134,6 @@ sub getPrimaryKeyPrefix {
 }
 
 
-
-=head
-sub makePrimaryKey {
-    my ($self, $hash) = @_;
-    
-    my $file =  basename $self->getMetadataFile();
-    my $date;
-    if ($file eq "gems1_huas_data.csv" || $file eq "gems1_huas_lite_data_kenya.csv"){
-	$date = $hash->{visitdate};  
-    }elsif($file eq "gems1_huas_lite_data_6_sites.csv"){
-        $date =  $hash->{visit_date};
-    }else{
-	die "could not find visitdate or visiot_date in these parent files";
-    }
-    $date= $self->formatdate($date);
-    return $date ? $hash->{childid} . "_" . $date : $hash->{childid};
-
-}
-=cut
 sub cleanAndAddDerivedData {
     my ($self, $hash) = @_;
     my $file =  basename $self->getMetadataFile();
@@ -161,12 +142,50 @@ sub cleanAndAddDerivedData {
 	if($file eq "gems1_huas_data.csv"){
 
 	    $hash->{huas_noseek_politic} = $hash->{noseek_politic}; 
+	    $hash->{noseek_politic} = undef;
+
 	}
 
 	if($file eq "gems1_huas_lite_data_kenya.csv" || $file eq "gems1_huas_lite_data_6_sites.csv"){
 	
 	    $hash->{kenya6sites_noseek_politic} = $hash->{noseek_politic};
+	    $hash->{noseek_politic} = undef;
+	    
 	}
+	if ($file eq "gems1_huas_lite_data_kenya.csv"){
+	    $hash->{vomit_kenya} = $hash->{vomit};
+	    $hash->{vomit} = undef;
+
+	    $hash->{seekcare_kenya} = $hash->{seekcare};
+	    $hash->{seekcare} = undef;
+
+	    $hash->{admit_kenya} = $hash->{admit};
+	    $hash->{admit} = undef;
+
+	    $hash->{offer_drink_kenya} = $hash->{offer_drink};
+	    $hash->{offer_drink} = undef;
+
+	    $hash->{offer_eat_kenya} = $hash->{offer_eat};
+	    $hash->{offer_eat} = undef;
+
+	}
+	if ($file eq "gems1_huas_lite_data_6_sites.csv"){
+            $hash->{vomit_6site} = $hash->{vomit};
+	    $hash->{vomit} = undef;
+
+	    $hash->{seekcare_6site} = $hash->{seekcare};
+	    $hash->{seekcare} = undef; 
+
+	    $hash->{admit_6site} = $hash->{admit};
+	    $hash->{admit} = undef; 
+
+	    $hash->{offer_drink_6site} = $hash->{offer_drink};
+	    $hash->{offer_drink} = undef;
+
+	    $hash->{offer_eat_6site} = $hash->{offer_eat};
+	    $hash->{offer_eat} = undef;
+
+        }
     }
 }
 
@@ -238,8 +257,6 @@ sub cleanAndAddDerivedData {
 	$hash->{kenya_consent} = $hash->{consent};
 	$hash->{consent} = undef;
 
-
-
     }else{
 	die "cannot find these files."
     }
@@ -271,29 +288,72 @@ sub getPrimaryKeyPrefix {
     return "O";
 }
 
+
+
+sub cleanAndAddDerivedData {
+    my ($self, $hash) = @_;
+    
+    my $file =  basename $self->getMetadataFile();
+    
+    if ($file eq "gems1a_huas_lite_data_6_sites.csv"){
+	$hash->{study_arm}="HUAS Lite";
+	
+	$hash->{diarrhea_6site} = $hash->{diarrhea};
+        $hash->{diarrhea} = undef;
+
+	$hash->{offer_eat_6site} = $hash->{offer_eat};
+        $hash->{offer_eat} = undef;
+
+	$hash->{offer_drink_6site} = $hash->{offer_drink};
+        $hash->{offer_drink} = undef;
+
+	$hash->{vomit_6site} = $hash->{vomit};
+        $hash->{vomit} = undef;
+
+	$hash->{seekcare_6site} = $hash->{seekcare};
+        $hash->{seekcare} = undef;
+
+	$hash->{admit_6site} = $hash->{admit};
+        $hash->{admit} = undef;
+
+
+    }elsif($file eq "gems1a_huas_lite_data_kenya.csv"){
+	$hash->{study_arm}="HUAS Lite - Kenya";
+
+
+	$hash->{kenya_consent} = $hash->{consent};
+	$hash->{consent} = undef;
+
+	$hash->{diarrhea_kenya} = $hash->{diarrhea};
+        $hash->{diarrhea} = undef;
+
+	$hash->{offer_eat_kenya} = $hash->{offer_eat};
+        $hash->{offer_eat} = undef;
+
+	$hash->{offer_drink_kenya} = $hash->{offer_drink};
+        $hash->{offer_drink} = undef;
+
+	$hash->{vomit_kenya} = $hash->{vomit};
+        $hash->{vomit} = undef;
+
+	$hash->{seekcare_kenya} = $hash->{seekcare};
+        $hash->{seekcare} = undef;
+
+	$hash->{admit_kenya} = $hash->{admit};
+        $hash->{admit} = undef;
+
+    }else{
+	die "cannot find these files."
+    }
+}
+
+
+
+
+
+
 1;
 
-=head
-sub makePrimaryKey {
-    my ($self, $hash) = @_;
 
-    my $file =  basename $self->getMetadataFile();
-
-    my $date;
-
-    if ($file eq "gems1a_huas_lite_data_6_sites.csv"){
-	
-	$date = $hash->{visit_date};  
-    }elsif($file eq "gems1a_huas_lite_data_kenya.csv"){
-	
-        $date =  $hash->{visitdate};
-    }else{
-	die "could not find visitdate or visiot_date in these parent files";
-    }
-
-    $date= $self->formatdate($date);
-    return $date ? $hash->{childid} . "_" . $date : $hash->{childid};
-}
-=cut
 
 

7530de2d3a66b01cc259e76729e7b491e269e7a7
Jay Humphrey <jaycolin@apidb.org>
2019-06-07 20:28:42 +0000

    revert bad copy/paste; deal with hhid
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiData/trunk@98749 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/Prism2Reader.pm
+++ Load/lib/perl/Prism2Reader.pm
@@ -1,194 +1,4 @@
 package ClinEpiData::Load::Prism2Reader;
-package ClinEpiData::Load::Prism2Reader;
-use base qw(ClinEpiData::Load::MetadataReader);
-use Data::Dumper;
-
-
-sub filterNaRows {
-  my ($self, $hash) = @_;
-  if(
-    $hash->{visittype} eq 'na' && 
-    $hash->{timetobed} eq 'na' &&
-    $hash->{participantdie} eq 'na'
-  ){
-    #printf STDERR ("Skipping NA row %s\n", $hash->{uniqueid}|| "UNDEF");
-    $self->skipRow($hash);
-  }
-}
-1;
-
-package ClinEpiData::Load::Prism2Reader::HouseholdReader;
-use base qw(ClinEpiData::Load::Prism2Reader);
-
-use strict;
-
-sub cleanAndAddDerivedData {
-  my ($self, $hash) = @_;
-  if(defined($hash->{llin})){
-    $hash->{household_llin} = $hash->{llin};
-    delete($hash->{llin});
-  }
-}
-
-sub makeParent {
-  return undef;
-}
-
-sub makePrimaryKey {
-  my ($self, $hash) = @_;
-
-  if($hash->{primary_key}) {
-    return $hash->{primary_key};
-  }
-
-  return $hash->{hhid};
-}
-
-
-1;
-
-package ClinEpiData::Load::Prism2Reader::ParticipantReader;
-use base qw(ClinEpiData::Load::Prism2Reader);
-use POSIX;
-
-use strict;
-
-sub makeParent {
-  my ($self, $hash) = @_;
-  if($hash->{parent}) {
-    return $hash->{parent};
-  }
-
-  return $hash->{hhid};
-}
-
-sub makePrimaryKey {
-  my ($self, $hash) = @_;
-
-  if($hash->{primary_key}) {
-    return $hash->{primary_key};
-  }
-
-  return $hash->{cohortid};
-}
-sub cleanAndAddDerivedData {
-  my ($self, $hash) = @_;
-  foreach my $col ( qw/reenrolldate dow lastdate cause3 death participantdie r2malaria withdrawnreason/ ){
-    next unless $hash->{$col} eq 'na';
-    delete($hash->{$col});
-  }
-}
-
-
-1;
-
-package ClinEpiData::Load::Prism2Reader::ObservationReader;
-use base qw(ClinEpiData::Load::Prism2Reader);
-
-use strict;
-sub cleanAndAddDerivedData {
-  my ($self, $hash) = @_;
-  $self->filterNaRows($hash);
-  my %hack = (374621500 => 1, 374721500 => 1, 374921500 => 1);
-  if($hack{$hash->{uniqueid}}){
-    delete($hash->{totalanopheles});
-  }
-}
-
-sub makePrimaryKey {
-  my ($self, $hash) = @_;
-  if($hash->{primary_key}) {
-    return $hash->{primary_key};
-  }
-  return $hash->{uniqueid};
-}
-
-
-sub makeParent {
-  my ($self, $hash) = @_;
-  if($hash->{parent}) {
-    return $hash->{parent};
-  }
-
-  return $hash->{cohortid};
-}
-
-1;
-
-
-
-package ClinEpiData::Load::Prism2Reader::LightTrapReader;
-use base qw(ClinEpiData::Load::Prism2Reader);
-
-use strict;
-
-my %type = (
-  'cdc light trap' => 'clt',
-  'resting collections' => 'rt'
-);
-
-sub cleanAndAddDerivedData {
-  my ($self, $hash) = @_;
-
-  if($hash->{date}) {
-    $hash->{collectiondate} = $hash->{date};
-  }
-}
-
-
-sub makePrimaryKey {
-  my ($self, $hash) = @_;
-  if($hash->{primary_key}) {
-    return $hash->{primary_key};
-  }
-  #return join("_",$hash->{hhid}, $hash->{date}, $type{$hash->{trapmethod}},$hash->{roomnumm});
-  return join("_",$hash->{hhid}, $hash->{date});
-}
-
-
-sub makeParent {
-  my ($self, $hash) = @_;
-  if($hash->{parent}) {
-    return $hash->{parent};
-  }
-  return $hash->{hhid};
-}
-
-1;
-
-package ClinEpiData::Load::Prism2Reader::SampleReader;
-use base qw(ClinEpiData::Load::Prism2Reader);
-
-use strict;
-
-sub cleanAndAddDerivedData {
-  my ($self, $hash) = @_;
-  $self->filterNaRows($hash);
-}
-
-sub makePrimaryKey {
-  my ($self, $hash) = @_;
-  if($hash->{primary_key}) {
-    return $hash->{primary_key};
-  }
-  return $hash->{uniqueid};
-}
-
-sub getPrimaryKeyPrefix {
-  return "S";
-}
-
-
-sub makeParent {
-  my ($self, $hash) = @_;
-  if($hash->{parent}) {
-    return $hash->{parent};
-  }
-
-  return $hash->{uniqueid};
-}
-
-1;
 
 use base qw(ClinEpiData::Load::MetadataReader);
 use Data::Dumper;
@@ -309,6 +119,7 @@ sub makeParent {
 
 package ClinEpiData::Load::Prism2Reader::LightTrapReader;
 use base qw(ClinEpiData::Load::Prism2Reader);
+use Data::Dumper;
 
 use strict;
 
@@ -332,7 +143,8 @@ sub makePrimaryKey {
     return $hash->{primary_key};
   }
   #return join("_",$hash->{hhid}, $hash->{date}, $type{$hash->{trapmethod}},$hash->{roomnumm});
-  return join("_",$hash->{hhid}, $hash->{date});
+  #return join("_",$hash->{hhid}, $hash->{date});
+  return $hash->{trap};
 }
 
 
@@ -341,7 +153,7 @@ sub makeParent {
   if($hash->{parent}) {
     return $hash->{parent};
   }
-  return $hash->{hhid};
+  return substr($hash->{trap},0,9);
 }
 
 1;

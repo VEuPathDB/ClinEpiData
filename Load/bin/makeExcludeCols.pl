b32bbe302a6dae8d5d796acd0c2867bbb9a30e05
Jay Humphrey <jaycolin@apidb.org>
2019-07-17 13:57:19 +0000

    support dataset::var format for variables, use -d::
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiData/trunk@99636 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/bin/makeExcludeCols.pl
+++ Load/bin/makeExcludeCols.pl
@@ -11,31 +11,32 @@ use Getopt::Long;
 use Data::Dumper;
 use CBIL::Util::PropertySet;
 
-my ($dataset, @filters, @files, $inverse, $propFile);
+my ($dataset, @filters, @files, $inverse, $propFile, $varDelim);
 GetOptions(
-	'o|owl=s' => \$dataset,
-	'f|filter=s' => \@filters,
-	'i|input=s' => \@files,
-	'v|inverse' => \$inverse,
-	'p|propFile=s' => \$propFile
+  'o|owl=s' => \$dataset,
+  'f|filter=s' => \@filters,
+  'i|input=s' => \@files,
+  'v|inverse' => \$inverse,
+  'p|propFile=s' => \$propFile,
+  'd|varDelim=s' => \$varDelim # a delimeter used in the owl file as in <column><Delim><file>
 );
-if(-e $propFile) {
-	my @_p;
+if(defined($propFile) && -e $propFile) {
+  my @_p;
   my $p = CBIL::Util::PropertySet->new($propFile, \@_p, 1);
-	push(@files, split(/,/,$p->{props}->{metadataFile}));
-	$dataset ||= $p->{props}->{ontologyOwlFile};
-	push(@filters, lc($p->{props}->{type})) unless @filters;
+  push(@files, split(/,/,$p->{props}->{metadataFile}));
+  $dataset ||= $p->{props}->{ontologyOwlFile};
+  push(@filters, lc($p->{props}->{type})) unless @filters;
 }
 
 unless($dataset){
-	printf(join("\n\n",
-		"Usage:\n\t%s -o|owl [owl] -f|filter [[filter]] -i|input [data file] [ -i [data file ] ] [-v|inverse]",
-		"Owl file must exist:\$PROJECT_HOME/ApiCommonData/Load/ontology/release/production/[owl].owl",
-		"Run without [[filter]] to see a list of options for this dataset",
-		"Run without -i[[data files ...]] to print only columns that are mapped in this dataset",
-		"Run with -v to get only columns in [[filter]]\n"
-	), basename($0));
-	exit;
+  printf(join("\n\n",
+    "Usage:\n\t%s -o|owl [owl] -f|filter [[filter]] -i|input [data file] [ -i [data file ] ] [-v|inverse]",
+    "Owl file must exist:\$PROJECT_HOME/ApiCommonData/Load/ontology/release/production/[owl].owl",
+    "Run without [[filter]] to see a list of options for this dataset",
+    "Run without -i[[data files ...]] to print only columns that are mapped in this dataset",
+    "Run with -v to get only columns in [[filter]]\n"
+  ), basename($0));
+  exit;
 }
 
 unless (@files) { $inverse = 1; } 
@@ -43,28 +44,33 @@ unless (@files) { $inverse = 1; }
 my %columns; ## all columns in data files
 my %index; ## col => file
 foreach my $file (@files){
-	unless(-f $file){
-		print STDERR "File does not exist: $file\n";
-		next;
-	}
-	open(FH, "<$file") or die "Cannot open $file:$!\n";
-	my $head = <FH>;
-	close(FH);
-	chomp $head;
-	$head =~ s/\r$//;
-	my $delim = ",";
-	$delim = "\t" if($head =~ /\t/);
-	for my $col (split(/$delim/, $head)){
-		$columns{pp($col)} = 1;
-		$index{pp($col)} = $file;
-	} 
+  unless(-f $file){
+    print STDERR "File does not exist: $file\n";
+    next;
+  }
+  open(FH, "<$file") or die "Cannot open $file:$!\n";
+  my $head = <FH>;
+  close(FH);
+  my ($baseName) = fileparse(lc($file), qr/\.[^.]+$/);
+  chomp $head;
+  $head =~ s/\r$//;
+  my $delim = ",";
+  $delim = "\t" if($head =~ /\t/);
+  for my $col (split(/$delim/, $head)){
+    $col = pp($col);
+    if($varDelim){ $col = join($varDelim, $baseName, $col); }
+    $columns{$col} = 1;
+    $index{$col} ||= {};
+    $index{$col}->{$baseName} = 1;
+ printf STDERR ("READ: $col FROM $baseName\n");
+  } 
 }
 
 printf STDERR ("%d files, %d columns\n", scalar @files, scalar keys %columns);
 
 my $owlFile = $dataset;
 unless( -e $owlFile ){
-	$owlFile = "$PROJECT_HOME/ApiCommonData/Load/ontology/release/production/$dataset.owl";
+  $owlFile = "$PROJECT_HOME/ApiCommonData/Load/ontology/release/production/$dataset.owl";
 }
 
 my $owl = ApiCommonData::Load::OwlReader->new($owlFile);
@@ -77,98 +83,102 @@ my %terms;
 my %filterOptions;
 my $it = $owl->execute('top_level_entities');
 while (my $row = $it->next) {
-	my $label = pp($row->{label}->as_sparql);
-	my $entity = $row->{entity}->as_sparql;
-	$filterOptions{$label} = $entity;
+  my $label = pp($row->{label}->as_sparql);
+  my $entity = $row->{entity}->as_sparql;
+  $filterOptions{$label} = $entity;
 }
 
 unless(@filters){
-	printf STDERR ("Choose one of these top-level entities:\n\t%s\n", join("\n\t", sort keys %filterOptions));
-	exit;
+  printf STDERR ("Choose one of these top-level entities:\n\t%s\n", join("\n\t", sort keys %filterOptions));
+  exit;
 } 
 
 foreach my $filter (@filters){
-	die "Entity not found for $filter\nAvailable:\n\t" . join("\n\t", keys %map) . "\n" unless defined $filterOptions{$filter};
-	my $filterEntity = $filterOptions{$filter};
+  die "Entity not found for $filter\nAvailable:\n\t" . join("\n\t", keys %map) . "\n" unless defined $filterOptions{$filter};
+  my $filterEntity = $filterOptions{$filter};
 
-	my $itr = $owl->execute('all_subclasses', { ENTITY => $filterEntity });
-	#my @keys = $itr->binding_names;
-	#printf ("%s\n", join("\t", @keys)) if @keys;
-	while (my $row = $itr->next) {
-		my $col = pp($row->{col}->as_sparql);
-		my @cols;
-		if($col =~ /,/){
-			@cols = split (/\s*,\s*/,$col);
-		}
-		else {
-			@cols = ($col);
-		}
-		foreach $col (@cols){
-			my $dataset = defined($row->{dataset}) ? pp($row->{dataset}->as_sparql) : [];
-			if(ref($dataset) eq 'ARRAY'){
-				$dataset = join("\t", @$dataset);
-			}
-			if(defined($terms{$col})){
-				if($terms{$col} eq '1' && $dataset){
-					$terms{$col} = $dataset;
-				}
-				elsif($dataset){
-					$terms{$col} .= $dataset;
-				}
-			}
-			elsif($dataset){
-				$terms{$col} = $dataset;
-			}
-			else {
-				$terms{$col} = 1;
-			}
-			if(defined($columns{$col})){
-				delete($columns{$col});
-				delete($index{$col});
-				$saved{$col} = 1;
-			}
-		}
-	}
+  my $itr = $owl->execute('all_subclasses', { ENTITY => $filterEntity });
+  #my @keys = $itr->binding_names;
+  #printf ("%s\n", join("\t", @keys)) if @keys;
+  while (my $row = $itr->next) {
+    my $col = pp($row->{col}->as_sparql);
+    my @cols;
+    if($col =~ /,/){
+      @cols = split (/\s*,\s*/,$col);
+    }
+    else {
+      @cols = ($col);
+    }
+    foreach $col (@cols){
+      my $dataset = defined($row->{dataset}) ? pp($row->{dataset}->as_sparql) : [];
+      if(ref($dataset) eq 'ARRAY'){
+        $dataset = join("\t", @$dataset);
+      }
+      if(defined($terms{$col})){
+        if($terms{$col} eq '1' && $dataset){
+          $terms{$col} = $dataset;
+        }
+        elsif($dataset){
+          $terms{$col} .= $dataset;
+        }
+      }
+      elsif($dataset){
+        $terms{$col} = $dataset;
+      }
+      else {
+        $terms{$col} = 1;
+      }
+      if(defined($columns{$col})){
+        delete($columns{$col});
+        delete($index{$col});
+        $saved{$col} = 1;
+      }
+    }
+  }
 
-	printf STDERR ("on branch %s(%s) there are %d terms\n", $filter, $filterEntity, scalar keys %terms);
-	printf STDERR ("%d columns remain\n", scalar keys %columns);
+  printf STDERR ("on branch %s(%s) there are %d terms\n", $filter, $filterEntity, scalar keys %terms);
+  printf STDERR ("%d columns remain\n", scalar keys %columns);
 }
 
 
 print STDERR ("-------------------\n");
 
 if($inverse){
-	foreach my $k (sort keys %saved){
-		if($terms{$k} eq '1'){
-			print "$k\n";
-		}
-		else {
-			print "$k\t$terms{$k}\n";
-		}
-	}
-	if( !  @files ){
-		foreach my $k (sort keys %terms){
-			if($terms{$k} eq '1'){
-				print "$k\n";
-			}
-			else {
-				print "$k\t$terms{$k}\n";
-			}
-		}
-	}
+  foreach my $k (sort keys %saved){
+    if($terms{$k} eq '1'){
+      print "$k\n";
+    }
+    else {
+      print "$k\t$terms{$k}\n";
+    }
+  }
+  if( !  @files ){
+    foreach my $k (sort keys %terms){
+      if($terms{$k} eq '1'){
+        print "$k\n";
+      }
+      else {
+        print "$k\t$terms{$k}\n";
+      }
+    }
+  }
 }
 else{
-	print "$_\n" for sort keys %columns;
-	## also exclude any columns not found in owl
+  foreach my $var (keys %index){
+    printf("%s\n", $var);
+   #foreach my $file( keys %{$index{$var}} ){
+   #  printf("%s\t%s\n", $var, $file);
+   #}
+  }
 }
 
 exit;
 
 
 sub pp {
-	my ($val) = @_;
-	$val =~ s/^"|"$//g;
-	return lc($val);
+  my ($val) = @_;
+  $val =~ s/^"|"$//g;
+  return lc($val);
 }
 
 1;

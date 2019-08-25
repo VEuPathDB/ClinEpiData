c5a92c2229272d822f66435278e5ead6ad7dfd69
Jay Humphrey <jaycolin@apidb.org>
2019-08-25 02:09:23 +0000

    2019-08 update
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiData/trunk@100504 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Load/lib/perl/AfricaReader.pm
+++ Load/lib/perl/AfricaReader.pm
@@ -1,17 +1,17 @@
 package ClinEpiData::Load::AfricaReader;
 use base qw(ClinEpiData::Load::MetadataReader);
 
-sub cleanAndAddDerivedData {
-  my ($self, $hash) = @_;
-	$self->fixDate($hash);
-}
-
-sub fixDate {
-  my ($self, $hash) = @_;
-	if($hash->{today} =~ /^(\d\d)-(...)-(\d\d)$/){
-		$hash->{today} = join("", $1, $2, '20', $3);
-	}
-}
+# sub cleanAndAddDerivedData {
+#   my ($self, $hash) = @_;
+# #	$self->fixDate($hash);
+# }
+# 
+# # sub fixDate {
+# #   my ($self, $hash) = @_;
+# # 	if($hash->{today} =~ /^(\d\d)-(...)-(\d\d)$/){
+# # 		$hash->{today} = join("", $1, $2, '20', $3);
+# # 	}
+# # }
 
 1;
 
@@ -19,10 +19,10 @@ package ClinEpiData::Load::AfricaReader::HouseholdReader;
 use base qw(ClinEpiData::Load::AfricaReader);
 
 
-sub cleanAndAddDerivedData {
-  my ($self, $hash) = @_;
-	$self->SUPER::cleanAndAddDerivedData($hash);
-}
+# sub cleanAndAddDerivedData {
+#   my ($self, $hash) = @_;
+# 	$self->SUPER::cleanAndAddDerivedData($hash);
+# }
 
 sub makeParent {
   return undef; 
@@ -30,11 +30,65 @@ sub makeParent {
 sub makePrimaryKey {
   my ($self, $hash) = @_;
 
+  if(defined($hash->{primary_key})) {
+    return $hash->{primary_key};
+  }
+  return $hash->{hh_number};
+}
+
+sub getPrimaryKeyPrefix {
+  my ($self, $hash) = @_;
+
+  if(defined($hash->{primary_key})) {
+    return ;
+  }
+  return "hh";
+}
+
+1;
+package ClinEpiData::Load::AfricaReader::HouseholdObservationReader;
+use base qw(ClinEpiData::Load::AfricaReader);
+use Data::Dumper;
+
+# sub cleanAndAddDerivedData {
+#   my ($self, $hash) = @_;
+# 	$self->SUPER::cleanAndAddDerivedData($hash);
+# }
+
+sub makeParent {
+  my ($self, $hash) = @_;
+  if(defined($hash->{parent})) {
+    return $hash->{parent};
+  }
+  return $hash->{hh_number};
+}
+sub getParentPrefix {
+  my ($self, $hash) = @_;
+
+  if(defined($hash->{parent})) {
+    return ;
+  }
+  return "hh";
+}
+
+sub makePrimaryKey {
+  my ($self, $hash) = @_;
+
   if($hash->{primary_key}) {
     return $hash->{primary_key};
   }
+  my $date = $self->formatDate($hash->{today}, "US");
 
-  return $hash->{hh_number};
+  return join("_", $hash->{hh_number}, $date);
+}
+
+sub getPrimaryKeyPrefix {
+  my ($self, $hash) = @_;
+
+  if(defined($hash->{primary_key})) {
+    return ;
+  }
+  return "ho";
 }
 
 1;
@@ -50,6 +104,14 @@ sub makeParent {
   }
 
   return $hash->{hh_number};
+}
+sub getParentPrefix {
+  my ($self, $hash) = @_;
+
+  if(defined($hash->{parent})) {
+    return ;
+  }
+  return "hh";
 }
 
 sub makePrimaryKey {
@@ -91,9 +153,19 @@ sub makePrimaryKey {
   if($hash->{primary_key}) {
     return $hash->{primary_key};
   }
-	$self->fixDate($hash);
-	return join("_", $hash->{part_id}, $hash->{today}); 
+	#$self->fixDate($hash);
+	#return join("_", $hash->{part_id}, $hash->{today}); 
+	return $hash->{part_id}; 
 }
+
+sub getPrimaryKeyPrefix {
+  my ($self, $hash) = @_;
+  if($hash->{primary_key}) {
+    return;
+  }
+  return "o";
+}
+
 
 1;
 
@@ -106,7 +178,16 @@ sub makeParent {
   if(defined($hash->{parent})) {
     return $hash->{parent};
   }
-  return join("_", $hash->{part_id}, $hash->{today}); 
+	return $hash->{part_id}; 
+  #return join("_", $hash->{part_id}, $hash->{today}); 
+}
+
+sub getParentPrefix {
+  my ($self, $hash) = @_;
+  if(defined($hash->{parent})) {
+    return ;
+  }
+  return "o";
 }
 
 sub makePrimaryKey {
@@ -114,12 +195,17 @@ sub makePrimaryKey {
   if($hash->{primary_key}) {
     return $hash->{primary_key};
   }
-	$self->fixDate($hash);
-  return join("_", $hash->{part_id}, $hash->{today}); 
+	return $hash->{part_id}; 
+	#$self->fixDate($hash);
+  #return join("_", $hash->{part_id}, $hash->{today}); 
 }
 
-sub getPrimaryKeySuffix {
-	return "S";
+sub getPrimaryKeyPrefix {
+  my ($self, $hash) = @_;
+  if(defined($hash->{primary_key})) {
+    return ;
+  }
+  return "s";
 }
 
 1;

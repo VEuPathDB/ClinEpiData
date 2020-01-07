2eebf611edf11f5fc13928005d6242a177dbff62
Lin Xu <linxu123@elm.pcbi.upenn.edu>
2020-01-07 12:18:03 -0500

    update Amazonia Brazil reader

--- Load/lib/perl/IcemrAmazoniaBrazilReader.pm
+++ Load/lib/perl/IcemrAmazoniaBrazilReader.pm
@@ -26,19 +26,24 @@ sub makeParent {
 
 sub makePrimaryKey {
   my ($self, $hash) = @_;
+  #my $metadataFilename=$self->getMetadataFileLCB();
+
   return $hash->{dom};
+  #return $hash->{ $metadataFilename . "::dom" };
 }
 
 sub getPrimaryKeyPrefix {
     return "HH"; 
 }
 
+
+=head
 sub cleanAndAddDerivedData{
     my ($self,$hash)=@_;
     $hash->{hhobservationprotocol}="Enrollment";
 
 }
-
+=cut
 1;
 
 
@@ -49,7 +54,7 @@ use base qw(ClinEpiData::Load::IcemrAmazoniaBrazilReader);
 
 sub makeParent {
   my ($self, $hash) = @_;
-  return $hash->{childid};
+  return $hash->{dom};
 }
 
 sub getParentPrefix {                                                                                                            
@@ -60,21 +65,108 @@ sub getParentPrefix {
 
 sub makePrimaryKey {                                                                                                             
     my ($self, $hash) = @_;                                                                                                      
-    my $suffix = $self->getSuffix();                                                                                             
-    return $hash->{childid} .  "_" . $suffix;                                                                                     
-}
+    #my $suffix = $self->getSuffix();                                                                                             
+    return $hash->{dom} .  "_" . $hash->{wave};                                                                                    }
+
+sub cleanAndAddDerivedData{                                                                                                       
+    my ($self,$hash)=@_; 
+    if ($hash->{dom} eq "61b" || $hash->{dom} eq "35a" ) { for my $key ( keys %$hash ) { $hash->{$key} =~ s/^na$//; } } 
+}     
+
+
+=head
 sub getSuffix{                                                                                                                    
-    return "household_followup";                                                                                                  
+    my ($self, $hash) = @_;                                                                                                      
+
+    return $hash->{wave};                                                                                                
 }
+=cut
 
-
+=head
 sub cleanAndAddDerivedData{
     my ($self,$hash)=@_;
-    $hash->{hhobservationprotocol}="60 day follow-up";
+    if ($hash->{wave} == 1){$hash->{wave}="enrollment";}
+    else $hash->{wave}="followup";
 
 
+}
+=cut
+1;
+
+
+package ClinEpiData::Load::IcemrAmazoniaBrazilReader::OutputReader;
+use base qw(ClinEpiData::Load::OutputFileReader);
+
+1;
+
+package ClinEpiData::Load::IcemrAmazoniaBrazilReader::ParticipantReader;
+use base qw(ClinEpiData::Load::IcemrAmazoniaBrazilReader);
+use File::Basename;
+use Data::Dumper;
+
+
+sub makeParent {
+    my ($self, $hash) = @_;
+    return $hash->{dominicial};
+}
+
+sub makePrimaryKey {
+    my ($self, $hash) = @_;
+    return $hash->{rg};
+}
+
+
+sub getParentPrefix {
+    my ($self, $hash) = @_;
+    return "HH";
 }
 
 1;
 
 
+package ClinEpiData::Load::IcemrAmazoniaBrazilReader::ObservationReader;
+use base qw(ClinEpiData::Load::IcemrAmazoniaBrazilReader);
+use File::Basename;
+use Data::Dumper;
+
+sub makeParent {
+  my ($self, $hash) = @_;
+  return $hash->{rg};
+}
+
+sub makePrimaryKey {                                                                                                             
+    my ($self, $hash) = @_;                                                                                                      
+    return $hash->{rg} .  "_" . $hash->{wave};  
+}
+
+sub getPrimaryKeyPrefix {
+    return "O_";
+}
+
+1;
+
+
+package ClinEpiData::Load::IcemrAmazoniaBrazilReader::SampleReader;
+use base qw(ClinEpiData::Load::IcemrAmazoniaBrazilReader);
+use File::Basename;
+use Data::Dumper;
+
+sub makeParent {
+    my ($self, $hash) = @_;
+    return $hash->{rg} .  "_" . $hash->{wave};
+}
+
+sub getParentPrefix {
+    return "O_";
+}
+
+sub makePrimaryKey {
+    my ($self, $hash) = @_;
+    return $hash->{rg} .  "_" . $hash->{wave};
+}
+
+sub getPrimaryKeyPrefix {
+    return "S_";
+}
+
+1;

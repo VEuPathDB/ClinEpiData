68565a1e85d3f39d6be572198a2ceead3acf264c
Jay Humphrey <jaycolin@apidb.org>
2020-02-21 14:07:00 -0500

    various improvements, in development

--- Load/lib/perl/GenericReader.pm
+++ Load/lib/perl/GenericReader.pm
@@ -4,7 +4,7 @@ use base qw(ClinEpiData::Load::MetadataReaderXT);
 use strict;
 use warnings;
 use Data::Dumper;
-my $DEBUG=0;
+my $DEBUG=1;
 
 sub updateConfig {
   my ($self) = @_;
@@ -16,23 +16,20 @@ sub updateConfig {
     foreach my $row (<FH>){
       chomp $row;
       next unless length($row);
-      my($mdfile,$type,$col) = split(/\t/, $row);
+      my($mdfile,$type,$col) = map { lc($_) } split(/\t/, $row);
       $mdfile = lc($mdfile);
-      my @types = split(/\//,lc($type));
-      my @cols = split(/\//,lc($col));
-      for (my $i = 0; $i < @types; $i++){
-        unless($types[$i] && $cols[$i]){ print STDERR "WARN: check syntax on row:\n$row\n" }
-        my @idCols;
-        if($noFilePrefix){
-          (@idCols) = split(/\+/, $cols[$i]);
-        }
-        else{
-          (@idCols) = map { join("::", $mdfile, $_) } split(/\+/, $cols[$i]);
-        }
-        $idMap->{$mdfile}->{$types[$i]} = \@idCols;
-        $idMap->{idHash}->{$mdfile}->{$types[$i]}->{$_} = 1 for @idCols;
-        
+      my @idCols;
+      if($noFilePrefix){
+        @idCols = split(/\+/, $col);
       }
+      else{
+        foreach my $c (split(/\+/, $col)){
+          if($c =~ /^\{\{.*\}\}$/){ push(@idCols, $c) }
+          else { push(@idCols, join("::", $mdfile, $c)) }
+        }
+      }
+      $idMap->{$mdfile}->{$type} = \@idCols;
+      $idMap->{idHash}->{$mdfile}->{$type}->{$_} = 1 for @idCols;
     }
     close(FH);
     $self->setConfig('idMap', $idMap);
@@ -57,31 +54,44 @@ sub getId {
     $self->setConfig('_warnings',$warnings);
   }
   my $mdfile = $self->getMetadataFileLCB();
-  die "$mdfile has no ID mapping" unless $mdfile;
+  # die "$mdfile has no ID mapping" unless $mdfile;
   unless(defined($idMap->{$mdfile}->{$type})){
     my $warn = "WARNING: $type not defined for $mdfile";
     printf STDERR ("$warn\n") unless $warnings->{$warn};
     $warnings->{$warn} = 1;
     return undef;
   }
-  #"$type ID mapping not defined in $mdfile" unless defined($idMap->{$mdfile}->{$type});
+  print STDERR "$type ID mapping not defined in $mdfile" unless defined($idMap->{$mdfile}->{$type});
   my @idCols = @{ $idMap->{$mdfile}->{$type} };
   foreach my $col (@idCols){
     next if(defined($hash->{$col}));
+    next if($col =~ /^\{\{.*\}\}$/);
     my $warn = "$col not defined in $mdfile for $type";
     printf STDERR ("$warn\n") unless $warnings->{$warn};
     $warnings->{$warn} = 1;
   }
   my @idValues;
   foreach my $col (@idCols){
-    my $val = $hash->{$col};
-    if(defined($placeholder) && ! defined($val)){
+    my $val = "UnDeF";
+    if($col =~ /^\{\{.*\}\}$/){
+      ($val) = ($col =~ m/^\{\{(.*)\}\}$/);
+    }
+    elsif($col =~ /\{\{/){
+      die "$col did not match\n"
+    }
+    else {
+      $val = $hash->{$col};
+    }
+    if(!(defined($val) && length($val)) && defined($placeholder)){
       $val = $placeholder;
+    }
+    unless(defined($val)){
+      die "ID Mapping invalid/missing for $type:\n\tFILE=$mdfile\n\t$col\nvalid columns:\n" . join("\n", keys %$hash);
     }
     push(@idValues,$val);
   }
   my $id = join("_", @idValues);
-  printf STDERR "ID: $id\n" if $DEBUG;
+  #printf STDERR "ID: $id\n" if $DEBUG;
   return $id;
 }
 
@@ -90,13 +100,10 @@ sub cleanAndAddDerivedData {
   return if defined $hash->{primary_key};
   my $type = lc($self->getConfig('type'));
   my $renameColumns = $self->getConfig('renameColumns');
+  $renameColumns ||= $self->getConfig('applyMappedIRI');
+  my $doValueMapping = $self->getConfig('doValueMapping');
+  $doValueMapping ||= $self->getConfig('applyMappedValues');
   my $mdfile = $self->getMetadataFileLCB();
-# my $idMap = $self->getConfig('idMap');
-# unless(defined($idMap->{$mdfile}->{$type})){
-#   # print STDERR ("$type:$mdfile - deleting row\n");
-#   $self->skipRow($hash);
-#   return;
-# }
   my $noFilePrefix = $self->getConfig('noFilePrefix');
   unless($noFilePrefix){
     foreach my $var (keys %$hash){
@@ -106,6 +113,9 @@ sub cleanAndAddDerivedData {
   }
   if($renameColumns){
     $self->applyMappedIRI($hash,1);
+  }
+  if($doValueMapping){
+    $self->applyMappedValues($hash,1);
   }
 }
 
@@ -176,5 +186,20 @@ use base qw(ClinEpiData::Load::GenericReader::CategoryReader);
 
 package ClinEpiData::Load::GenericReader::SampleReader;
 use base qw(ClinEpiData::Load::GenericReader::CategoryReader);
+
+1;
+
+package ClinEpiData::Load::GenericReader::OutputReader;
+use base qw(ClinEpiData::Load::MetadataReader);
+
+sub makeParent {
+  my ($self, $hash) = @_;
+  return $hash->{parent};
+}
+
+sub makePrimaryKey {
+  my ($self, $hash) = @_;
+  return $hash->{primary_key};
+}
 
 1;

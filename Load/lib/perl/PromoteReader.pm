3d52ee4e25b28a05fed33572c07c18dedc0c9278
Jay Humphrey <jaycolin@apidb.org>
2020-03-12 10:49:56 -0400

    General PROMOTE reader for observations and samples needing rowMultiplier (others use Generic)

---
+++ Load/lib/perl/PromoteReader.pm
@@ -0,0 +1,34 @@
+package ClinEpiData::Load::PromoteReader;
+use base qw(ClinEpiData::Load::GenericReader);
+1;
+
+package ClinEpiData::Load::PromoteReader::OutputReader;
+use base qw(ClinEpiData::Load::GenericReader::OutputReader);
+
+1;
+package ClinEpiData::Load::PromoteReader::DeliveryReader;
+use base qw(ClinEpiData::Load::GenericReader::CategoryReader);
+
+use strict;
+use warnings;
+use Data::Dumper;
+
+sub  rowMultiplier {
+  my ($self, $row) = @_;
+  my $mdfile = $self->getMetadataFileLCB();
+  return [$row] if $mdfile ne 'bc-3 mothers delivery database final';
+  my @rows;
+  foreach my $inc ( 1 .. 7 ){
+  # id+hospdate+ldate1+ltimehrs+ltimemin
+    my %clone;
+    map { $clone{$_} = $row->{$_} } grep { !/ldate|ltime/ } keys %$row;
+    foreach my $var (qw/ldate ltimehrs ltimemin/){
+      my $varN = sprintf("%s%d", $var, $inc);
+      $clone{$var} = $row->{$varN} || "na";
+    }
+    push(@rows, \%clone);
+  }
+  return \@rows;
+}
+
+1;

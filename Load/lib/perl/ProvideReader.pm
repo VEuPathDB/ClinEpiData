84bfa9370513940c49479e0298d938e6fc31a1f5
Jay Humphrey <jaycolin@apidb.org>
2020-07-23 22:11:18 -0400

    Gates PROVIDE Reader

--- Load/lib/perl/ProvideReader.pm
+++ Load/lib/perl/ProvideReader.pm
@@ -1,84 +1,5 @@
 package ClinEpiData::Load::ProvideReader;
-use base qw(ClinEpiData::Load::MetadataReaderXT);
-
-use strict;
-use warnings;
-use Data::Dumper;
-
-sub updateConfig {
-  my ($self) = @_;
-  my $idMappingFile = $self->getConfig('idMappingFile');
-  if($idMappingFile){
-    open(FH, "<", $idMappingFile) or die "Cannot open $idMappingFile:$!\n";
-    my $idMap = {};
-    foreach my $row (<FH>){
-      chomp $row;
-      my($mdfile,$type,$col) = split(/\t/, $row);
-      my @types = split(/\//,lc($type));
-      my @cols = split(/\//,lc($col));
-      for (my $i = 0; $i < @types; $i++){
-        unless($types[$i] && $cols[$i]){ print STDERR "WARN: check syntax on row:\n$row\n" }
-        my(@idCols) = map { join("::", $mdfile, $_) } split(/\+/, $cols[$i]);
-        $idMap->{$mdfile}->{$types[$i]} = \@idCols;
-        $idMap->{idHash}->{$mdfile}->{$types[$i]}->{$_} = 1 for @idCols;
-        
-      }
-    }
-    close(FH);
-    $self->setConfig('idMap', $idMap);
-  # $self->setConfig('cleanFirst', 1);
-  }
-  my ($type) = map { lc($_) } (ref($self) =~ m/::([^:]*)Reader$/);
-  $self->setConfig('type', $type);
-} 
-
-sub getId {
-  my ($self, $hash, $type) = @_;
-  unless($type){
-    $type = $self->getConfig('type');
-  }
-  my $idMap = $self->getConfig('idMap');
-  my $warnings = $self->getConfig('_warnings');
-  unless($warnings){
-    $warnings = {};
-    $self->setConfig('_warnings',$warnings);
-  }
-  my $mdfile = $self->getMetadataFileLCB();
-  die "$mdfile has no ID mapping" unless $mdfile;
-  unless(defined($idMap->{$mdfile}->{$type})){
-    my $warn = "WARNING: $type not defined for $mdfile";
-    printf STDERR ("$warn\n") unless $warnings->{$warn};
-    $warnings->{$warn} = 1;
-    return undef;
-  }
-  #"$type ID mapping not defined in $mdfile" unless defined($idMap->{$mdfile}->{$type});
-  my @idCols = @{ $idMap->{$mdfile}->{$type} };
-  foreach my $col (@idCols){
-    next if(defined($hash->{$col}));
-    my $warn = "$col not defined in $mdfile for $type";
-    printf STDERR ("$warn\n") unless $warnings->{$warn};
-    $warnings->{$warn} = 1;
-  }
-  my $id = join("_", map { $hash->{$_} } @idCols);
-  return $id;
-}
-
-sub cleanAndAddDerivedData {
-  my ($self, $hash) = @_;
-  return if defined $hash->{primary_key};
-  my $type = lc($self->getConfig('type'));
-  my $idMap = $self->getConfig('idMap');
-  my $mdfile = $self->getMetadataFileLCB();
-# unless(defined($idMap->{$mdfile}->{$type})){
-#   # print STDERR ("$type:$mdfile - deleting row\n");
-#   $self->skipRow($hash);
-#   return;
-# }
-  foreach my $var (keys %$hash){
-    $hash->{join("::",$mdfile,$var) } = $hash->{$var};
-    delete($hash->{$var});
-  }
-}
+use base qw(ClinEpiData::Load::GenericReader);
 
 1;
 
@@ -105,13 +26,6 @@ sub getPrimaryKeyPrefix {
   }
 	return "h";
 }
-
-#sub cleanAndAddDerivedData {
-#  my ($self, $hash) = @_;
-#  if(defined($hash->{primary_key})){ return }
-#  $self->SUPER::cleanAndAddDerivedData($hash);
-#  $hash->{country} = 'Mali';
-#}
 
 1;
 
@@ -158,36 +72,11 @@ sub makePrimaryKey {
 
 
 package ClinEpiData::Load::ProvideReader::ObservationReader;
-use base qw(ClinEpiData::Load::ProvideReader);
+#use base qw(ClinEpiData::Load::ProvideReader);
+use base qw(ClinEpiData::Load::GenericReader::CategoryReader);
 use strict;
 use warnings;
 use Data::Dumper;
-
-sub makeParent {
-  my ($self, $hash) = @_;
-
-  if($hash->{parent}) {
-    return $hash->{parent};
-  }
-  return $self->getId($hash,'participant');
-}
-
-sub makePrimaryKey {
-  my ($self, $hash) = @_;
-
-  if(defined($hash->{primary_key})) {
-    return $hash->{primary_key};
-  }
-  return $self->getId($hash);
-}
-
-sub getPrimaryKeyPrefix {
-  my ($self, $hash) = @_;
-  if($hash->{primary_key}) {
-    return undef;
-  }
-	return "o";
-}
 
 sub cleanAndAddDerivedData {
   my ($self, $hash) = @_;
@@ -201,68 +90,57 @@ sub cleanAndAddDerivedData {
  #  $hash->{observation_date} = $self->formatDate($hash->{observation_date},"US");
  #}
 
- foreach my $var (qw/ext_bv_dir_st::dov ext_mgmt_bv_ddep_st::epidate ext_bv_dep_st::dedt ext_bv_dep_st::dedt/){
-   if(defined($hash->{$var})){
-     $hash->{$var} = $self->formatDate($hash->{$var},"US");
-   }
- }
+  my @dateVars = grep { /dt$|doc$|date$|dov$/ } keys %$hash;
+ #foreach my $var (qw/ext_bv_dir_st::dov ext_mgmt_bv_ddep_st::epidate ext_bv_dep_st::dedt ext_bv_dep_st::dedt ext_bv_ptreat_st::dov/){
+  foreach my $var (@dateVars){
+    if(defined($hash->{$var}) && length($hash->{$var}) && ($hash->{$var} ne "na")){
+      $hash->{$var} = $self->formatDate($hash->{$var},"US");
+      if($hash->{$var} eq '1999-09-09'){ $hash->{$var} = "" }
+    }
+  }
 
+  ## aeduration
+  if(defined($hash->{'ext_mgmt_bv_aed_st::aedt'}) && defined($hash->{'ext_mgmt_bv_aed_st::aeresdt'})){
+    $hash->{aeduration} = $self->dateDiff($hash->{'ext_mgmt_bv_aed_st::aedt'},$hash->{'ext_mgmt_bv_aed_st::aeresdt'});
+  }
+  elsif(defined($hash->{'ext_bv_sae_st::saerdt'}) && defined($hash->{'ext_bv_sae_st::saersdt'})){
+    $hash->{aeduration} = $self->dateDiff($hash->{'ext_bv_sae_st::saerdt'},$hash->{'ext_bv_sae_st::saersdt'});
+  }
 }
 
 1;
 
 package ClinEpiData::Load::ProvideReader::SampleReader;
-use base qw(ClinEpiData::Load::ProvideReader::ObservationReader);
-
-sub makeParent {
-  my ($self, $hash) = @_;
-  if($hash->{parent}) {
-    return $hash->{parent};
-  }
-  return $self->getId($hash,'observation');
-}
-
-sub getParentPrefix {
-  my ($self, $hash) = @_;
-  if($hash->{parent}) {
-    return undef;
-  }
-	return "o";
-}
-
-sub makePrimaryKey {
-  my ($self, $hash) = @_;
-
-  if($hash->{primary_key}) {
-    return $hash->{primary_key};
-  }
-  return $self->getId($hash);
-}
-
-sub getPrimaryKeyPrefix {
-  my ($self, $hash) = @_;
-  if($hash->{primary_key}) {
-    return undef;
-  }
-	return "s";
-}
+use base qw(ClinEpiData::Load::GenericReader::CategoryReader);
 
 sub cleanAndAddDerivedData {
   my ($self, $hash) = @_;
   if(defined($hash->{primary_key})){ return }
  #my $a = $hash->{vstnum};
+ $hash->{$_} =~ s/"//g for keys %$hash;
  $self->applyMappedValues($hash);
  #my $b = $hash->{vstnum};
  ##if($a && ($a ne $b))
  #{print STDERR ("remapped vstnum %s = %s\n", $a, $b) }
-  my @dateVars = grep { /dt$|^lmdoc$|^doc$/ } keys %$hash;
+  my @dateVars = grep { /dt$|doc$|date$|dov|dt18$/ } keys %$hash;
   foreach my $var (@dateVars){
+    next unless $hash->{$var};
     if(defined($hash->{$var}) && ($hash->{$var} ne "na")){
-      $hash->{$var} = $self->formatDate($hash->{$var},"US");
-      if($hash->{$var} eq '1999-09-09'){ delete $hash->{$var} }
+      my $date = $self->formatDate($hash->{$var},"US");
+      if(defined($date)){
+        if($hash->{$var} eq '1999-09-09'){ $hash->{$var} = "" }
+        else { $hash->{$var} = $date }
+      }
+      else{ print STDERR "cannot format date $hash->{$var}\n" }
+        
     }
   }
   $self->SUPER::cleanAndAddDerivedData($hash);
 }
+
+1;
+
+package ClinEpiData::Load::ProvideReader::OutputReader;
+use base qw(ClinEpiData::Load::GenericReader::OutputReader);
 
 1;

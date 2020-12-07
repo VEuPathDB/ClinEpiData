9a5be872fc85f86ccd5e6b63c6435e69c893ed16
Jay Humphrey <jaycolin@apidb.org>
2020-12-07 13:25:59 -0500

    Text::CSV wrappers

--- Load/lib/perl/Utilities/File.pm
+++ Load/lib/perl/Utilities/File.pm
@@ -1,28 +1,65 @@
 package ClinEpiData::Load::Utilities::File;
 require Exporter;
 our @ISA = qw/Exporter/;
-our @EXPORT_OK = qw/csv2tab diff/;
-use Text::CSV;
+our @EXPORT_OK = qw/csv2tab csv2array diff tabWriter/;
+use Text::CSV_XS;
 use Scalar::Util qw/looks_like_number/;
 use open ':std', ':encoding(UTF-8)';
 
-sub csv2tab {
-  my ($file, $out, $delim) = @_;
+sub tabWriter{
+  my $csv = Text::CSV_XS->new({binary => 1, sep_char => "\t", quote_char => '"' }) or die "Cannot use CSV: " . Text::CSV->error_diag ();
+  return $csv;
+}
+
+sub csv2array {
+  my ($file, $delim) = @_;
   $delim ||= ",";
-  my $csv = Text::CSV->new({binary => 1, sep_char => $delim, quote_char => '"' }) or die "Cannot use CSV: " . Text::CSV->error_diag ();  
+  my $csv = Text::CSV_XS->new({binary => 1, sep_char => $delim, quote_char => '"' }) or die "Cannot use CSV: " . Text::CSV->error_diag ();  
   open(my $ifh, "<$file") or die "$@\n";
-  open(my $ofh, ">$out") or die "$@\n";
+  my @rows;
   while (my $row = $csv->getline( $ifh )) {
   	$lines++;
   	#die if grep { /\t/ } @$row;
-  	my @out;
+  	my @data;
   	foreach my $val (@$row){
-      $val =~ s/^\x{FEFF}//;
-  		$val =~ s/[\n\r\l]/ /g;
-  		$val =~ s/\t/ /g;
-  		push(@out, $val);
+      $val =~ s/\x{FEFF}//;
+    	$val =~ s/[\n\r\l]/ /g;
+    	$val =~ s/\t/ /g;
+  		push(@data, $val);
   	}
-  	printf $ofh ("%s\n", join("\t", @$row));
+  	push(@rows, \@data);
+  }
+  close($ifh);
+  return \@rows;
+}
+
+sub csv2tab {
+  my ($file, $out, $delim) = @_;
+#  use open ':std', ':encoding(UTF-8)';
+  $delim ||= ",";
+  my $csv = Text::CSV_XS->new({binary => 1, sep_char => $delim, quote_char => '"' }) or die "Cannot use CSV: " . Text::CSV->error_diag ();  
+  open(my $ifh, "<$file") or die "$@\n";
+  open(my $ofh, ">$out") or die "$@\n";
+ #if(0){ # try to parse header containing BOM
+ #  my $header = <$ifh>;
+ #  chomp $header;
+ #  $header =~ s/^\xEF|^\xBB^\xBF//;
+ #  $header =~ s/[\n\r\l]//;
+ #  $csv->parse($header);
+ #  my @cols = $csv->fields;
+ # 	printf $ofh ("%s\n", join("\t", @cols));
+ #}
+  while (my $row = $csv->getline( $ifh )) {
+  	$lines++;
+  	#die if grep { /\t/ } @$row;
+  	my @data;
+  	foreach my $val (@$row){
+      $val =~ s/\x{FEFF}//;
+    	$val =~ s/[\n\r\l]/ /g;
+    	$val =~ s/\t/ /g;
+  		push(@data, $val);
+  	}
+  	printf $ofh ("%s\n", join("\t", @data));
   }
   close($ifh);
   close($ofh);
@@ -58,11 +95,11 @@ sub diff {
         $info{removedValue}++;
         next;
       }
-      if($v eq 'NA'){$v = ''}
+      $v =~ s/^\s*|\s*$//g;
+      if($v =~ /^N[\/]?A$/i){$v = ''}
       my $val = $B->{$id}->{$k};
-      if($val eq 'NA'){
-        $val='';
-      }
+      $val =~ s/^\s*|\s*$//g;
+      if($val =~ /^N[\/]?A$/i){$val = ''}
       if($val ne $v){
         if(looks_like_number($val)
           && looks_like_number($v)
@@ -113,7 +150,7 @@ sub readFileGetVarsAndDataHashes {
   my ($file,$delim) = @_;
   $delim ||= "\t";
   msg("reading $file");
-  my $csv = Text::CSV->new({binary => 1, quote_char => '"', sep_char => $delim});
+  my $csv = Text::CSV_XS->new({binary => 1, quote_char => '"', sep_char => $delim});
   open(my $fh, "<$file") or die "Cannot read $file:$!\n";
   my $hr = $csv->getline($fh);
   my $k = $hr->[0];

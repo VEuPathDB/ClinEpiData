98fc37c8f9abf741d361307147e72d9ae1cf6d54
Jay Humphrey <jaycolin@apidb.org>
2021-06-23 11:17:06 -0400

    Added validation; probably will move hardcoded tests into .ini

--- Load/bin/getEntityAttributesFromOwl.pl
+++ Load/bin/getEntityAttributesFromOwl.pl
@@ -8,9 +8,9 @@ use warnings;
 use lib $ENV{GUS_HOME} . "/lib/perl";
 
 use ApiCommonData::Load::OwlReader;
-
 use File::Basename;
-
+use Switch;
+use Scalar::Util qw/blessed looks_like_number/;
 use JSON qw/to_json/;
 
 my ($owlFile) = @ARGV;
@@ -50,6 +50,21 @@ variable => 1,
 displayType => 1, ## derived annotation property
 );
 
+# if a value passes ANY test, it passes validation
+my %validationTest = (
+defaultBinWidth => [qw/nonzero empty/],
+defaultDisplayRangeMax => [qw/numeric date empty/],
+defaultDisplayRangeMin => [qw/numeric date empty/],
+displayOrder => [qw/numeric empty/],
+# hidden => [qw/"yes" empty/],
+is_featured => [qw/"yes" empty/],
+is_temporal => [qw/"yes" empty/],
+mergeKey => [qw/"yes" empty/],
+repeated => [qw/"yes" empty/],
+# termType => [qw/category "multifilter" "value" "variable" "variable,derived" "value,derived"/],
+displayType => [qw/"hidden" "multifilter"/], ## derived from hidden and termType
+);
+
 my $it = $owl->execute('get_entity_attributes');
 while (my $row = $it->next) {
   my $termId = pp(basename($row->{entity}->as_sparql));
@@ -64,12 +79,12 @@ while (my $row = $it->next) {
 
 ## post-processing in this while loop
 while( my( $termId, $props ) = each %outputHashes ){
-  my $hidden = lc($props->{hidden}->[0]);
+  my $hidden = defined($props->{hidden}->[0]) ? lc($props->{hidden}->[0]) : '';
   my $termType = lc($props->{termType}->[0]);
   delete($outputHashes{$termId}->{termType});
   delete($outputHashes{$termId}->{hidden});
   next unless $hidden || $termType;
-  printf STDERR ("HIDDEN = $hidden, TERM_TYPE = $termType\n");
+  # printf STDERR ("HIDDEN = $hidden, TERM_TYPE = $termType\n");
   if( $hidden eq 'yes' ){
     $outputHashes{$termId}->{displayType} = ['hidden'];
   }
@@ -78,6 +93,32 @@ while( my( $termId, $props ) = each %outputHashes ){
   }
   # always delete hidden and termType
 }
+
+## do validation
+while( my ($termId, $termHash) = each %outputHashes){
+  while (my ($propName, $values) = each %$termHash){
+    next unless $validationTest{$propName};
+    foreach my $value (@$values){
+      my $score = scalar @{$validationTest{$propName}}; # value assumed to be valid
+      ## if $score reaches zero, all tests failed
+      ## otherwise, it passed at least one test
+      foreach my $test ( @{$validationTest{$propName}} ){ 
+        switch ($test){
+          case /^"(.+)"$/ { $score-- unless $value =~ /$1/ }
+          case 'numeric' { $score-- unless looks_like_number($value) }
+          case 'nonzero' { $score-- unless looks_like_number($value) && $value != 0 }
+          case 'date' { $score-- unless $value =~ /^\d{4}-\d{2}-\d{2}$/ }
+          case 'empty' { $score-- unless $value =~ /^$/ }
+        }
+      }
+      if($score <= 0){
+        printf STDERR ("FAIL: %s %s [%s] TESTS:%s\n",
+          $termId, $propName, $value, join(", ", @{$validationTest{$propName}}))
+      }
+    }
+  }
+}
+  
 
 my $max = 0;
 
@@ -97,3 +138,6 @@ sub pp {
   $val =~ s/^"|"$//g;
   return $val;
 }
+
+
+

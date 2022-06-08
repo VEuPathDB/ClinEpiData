6e281b96173d333dc7d94317f806cc48222f9e05
Jay Humphrey <jaycolin@apidb.org>
2022-06-08 17:23:24 -0400

    Some tests that were implemented in clsh

--- Load/bin/getOrdinalValuesFromValueMap.pl
+++ Load/bin/getOrdinalValuesFromValueMap.pl
@@ -20,15 +20,30 @@ foreach my $row( @$valueMap ){
   $_line++;
   my ($var, $iri, $orig, $val, $order) = @$row;
   next unless (defined($iri) && defined($val) && defined($order) && $order ne "");
+  my $score = 1;
   unless(looks_like_number($order)){
     printf STDERR ("ERROR: LINE $_line: '$order' is not a number, skipping\n");
     next;
   }
   # warn if already inserted with a different ordering
   if(defined($perms{$iri}->{ $val }) && $perms{$iri}->{ $val } != $order){
-    printf STDERR ("ERROR: LINE $_line: $iri $val does not match previous: %s\n", $perms{$iri}->{$val});
+    printf STDERR ("ERROR: LINE $_line: $iri $val ordinal value '%s' does not match previous: %s\n", $order, $perms{$iri}->{$val});
+    $score--;
   }
-  $perms{$iri}->{$val} = $order;
+  if ( looks_like_number($val) ){
+    printf STDERR ("WARNING: LINE $_line: $iri $val looks like a number, this variable will be loaded as ORDINAL\n");
+    # no decrement of $score
+  }
+  if ( $val =~ /UNDEF/i ){
+    printf STDERR ("ERROR: LINE $_line: $iri $val cannot have an ordinal ($order)\n");
+    $score--;
+  }
+  if($score > 0){
+    $perms{$iri}->{$val} = $order;
+  }
+  else{
+    printf STDERR ("ERROR: LINE $_line: $iri $val FAILED VALIDATION\n");
+  }
 }
 
 if(0 < keys %perms){

a52e6c251a26ff0cbe4291153aa05022ed3cae4c
Jay Humphrey <jaycolin@apidb.org>
2022-11-01 14:28:49 -0400

    get project from workflow config; execute commands with -x

--- Load/bin/syncDownloadFiles.pl
+++ Load/bin/syncDownloadFiles.pl
@@ -4,12 +4,16 @@ use warnings;
 
 use Digest::SHA qw/sha1_hex/;
 use Config::Std; # for workflow.prop
+use Getopt::Long qw/:config no_ignore_case/;
 
+my ($execute);
+GetOptions ( 'x|execute!' => \$execute );
 my ($datasetName, $rel, $test) = @ARGV;
 
 unless( $ENV{WORKFLOW_DIR} ){ print "WORKFLOW_DIR not set\n"; exit 1; }
 read_config( $ENV{WORKFLOW_DIR} . "/config/workflow.prop", my %config );
 my $version = $config{''}->{version};
+my $proj = $config{''}->{name};
 
 unless(length($version)){ print "Cannot read version from WORKFLOW_DIR/config/workflow.prop"; exit 1; }
 
@@ -17,14 +21,14 @@ my $dsId = sha1_hex($datasetName);
 
 printf STDERR "Syncing $datasetName $dsId\n";
 
-my $downloadSource = "/eupath/data/apiSiteFilesStaging/ClinEpiDB/$version/real/downloadSite/ClinEpiDB/release-CURRENT/$dsId";
+my $downloadSource = "/eupath/data/apiSiteFilesStaging/$proj/$version/real/downloadSite/$proj/release-CURRENT/$dsId";
 
 if(-d $downloadSource){ 
   my @targets = (
-#    "elm:/eupath/data/apiSiteFilesStaging/ClinEpiDB/4.0/real/downloadSite/ClinEpiDB/release-CURRENT/",
-#   sprintf(  "elm:/eupath/data/apiSiteFiles/downloadSite/ClinEpiDB/release-%s/",$rel),
-    sprintf(  "/eupath/data/apiSiteFiles/downloadSite/ClinEpiDB/release-%s",$rel),
-#    sprintf("melon:/var/www/Common/apiSiteFilesMirror/downloadSite/ClinEpiDB/release-%s/",$rel)
+#    "elm:/eupath/data/apiSiteFilesStaging/$proj/4.0/real/downloadSite/$proj/release-CURRENT/",
+#   sprintf(  "elm:/eupath/data/apiSiteFiles/downloadSite/$proj/release-%s/",$rel),
+    sprintf(  "/eupath/data/apiSiteFiles/downloadSite/$proj/release-%s",$rel),
+#    sprintf("melon:/var/www/Common/apiSiteFilesMirror/downloadSite/$proj/release-%s/",$rel)
   );
   foreach my $target (@targets){
     my $cmd = sprintf("rsync -r -i --delete %s %s %s",
@@ -32,24 +36,36 @@ if(-d $downloadSource){
       $downloadSource,
       $target);
     printf ("$cmd\n");
+    if($execute){
+      my $status = system($cmd);
+      die if $status;
+    }
     # rename with release number
     # $cmd = "for file in `ls $target/$dsId`; do mv \'$target/$dsId/\$file\' \'$target/$dsId/rls00${rel}_\$file\'; done";
     $cmd = "rename $dsId/ $dsId/rls00${rel}_ $target/$dsId/*";
     printf ("$cmd\n");
-    printf "ls -1 $target/$dsId/";
+    if($execute){
+      my $status = system($cmd);
+      die if $status;
+    }
+    $cmd = "ls -1 $target/$dsId/";
+    if($execute){
+      my $status = system($cmd);
+      die if $status;
+    }
   }
 }
 else {
   printf STDERR ("Directory not found: $downloadSource\n");
 }
 =cut
-my $webServicesSource = "/eupath/data/apiSiteFilesStaging/ClinEpiDB/4.0/real/webServices/ClinEpiDB/release-CURRENT/$datasetName";
+my $webServicesSource = "/eupath/data/apiSiteFilesStaging/$proj/4.0/real/webServices/$proj/release-CURRENT/$datasetName";
 
 if(-d $webServicesSource){ 
   my @targets = (
-    "elm:/eupath/data/apiSiteFilesStaging/ClinEpiDB/4.0/real/webServices/ClinEpiDB/release-CURRENT/",
-    sprintf(  "elm:/eupath/data/apiSiteFiles/webServices/ClinEpiDB/release-%s/",$rel),
-    sprintf("melon:/var/www/Common/apiSiteFilesMirror/webServices/ClinEpiDB/release-%s/",$rel)
+    "elm:/eupath/data/apiSiteFilesStaging/$proj/4.0/real/webServices/$proj/release-CURRENT/",
+    sprintf(  "elm:/eupath/data/apiSiteFiles/webServices/$proj/release-%s/",$rel),
+    sprintf("melon:/var/www/Common/apiSiteFilesMirror/webServices/$proj/release-%s/",$rel)
   );
   foreach my $target (@targets){
     my $cmd = sprintf("rsync -r -i %s %s %s",
